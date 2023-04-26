194310463b169e786c24c2fa13af5d0999215f51
nghiaagent <nghia6a1@gmail.com>
2023-04-26 10:20:46 +1000

    replace code for loading packages with external script

--- scripts/master-script-transcriptome.R
+++ scripts/master-script-transcriptome.R
@@ -1,55 +1,7 @@
-# TRANSCRIPTOME
-# ------------------------------------------------LOAD PACKAGES----------------------------------------------------
-### Install pacman, a package manager to install and load multiple packages at once
+### Load all packages using script under ./scripts/pkg_load.R
+# If more packages are needed, please add them to pkg_load.R
 
-if (!requireNamespace("pacman")) {
-  install.packages("pacman")
-}
-library(pacman)
-
-### Some packages below use Bioconductor
-
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-library(BiocManager)
-
-### Load all packages with pacman
-
-p_load(readr, dplyr, tidyr, tibble)
-
-### List Bioconductor packages to be required
-
-list_Bioc_Pkg <- c(
-  "tximport",
-  "edgeR",
-  "Homo.sapiens",
-  "limma",
-  "ChAMP",
-  "biomaRt",
-  "goseq",
-  "qusage",
-  "maditr",
-  "Glimma",
-  "clusterProfiler",
-  "EnsDb.Hsapiens.v75",
-  "fgsea",
-  "ComplexHeatmap"
-)
-
-### Install Bioconductor packages, if they are not yet installed and/or not up-to-date
-
-BiocManager::install(
-  pkgs = list_Bioc_Pkg,
-  update = TRUE,
-  ask = FALSE,
-  force = FALSE
-)
-
-### Load Bioconductor packages with pacman
-
-invisible(lapply(list_Bioc_Pkg, function(x)
-  library(x, character.only = TRUE)))
+source(file = "./scripts/pkg_load.R")
 
 # ------------------------------------------------------------------------------------------------------------------
 setwd(
