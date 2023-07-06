3ca71a290f8bab79761fd24ceb713ea3da32a247
Nghia Nguyen <n10304070@qut.edu.au>
2023-07-06 14:50:06 +1000

    completed script for both ENSEMBL datasets

--- scripts/tx_quant_import.R
+++ scripts/tx_quant_import.R
@@ -2,50 +2,142 @@
 ### Source 
 ### Import salmon transcript quantification results to a single matrix, export that matrix.
 
-# tx2gene is a df linking transcript ID to gene ID Hence, "transcript to gene"
-# We use this to turn transcript quantification results to gene quant results.
-# Import tx2gene df
+# Construct sample table
 
-tx2gene <- read.csv("./input/Homo_sapiens.GRCh38.91_tx2gene.csv")
-head(tx2gene, 5)
+table_samples <- read_csv("./input/annotation/Cell_sample_table.csv") %>%
+  mutate(ID = name,
+         .keep = "unused") %>%
+  dplyr::select(!c(name1)) %>%
+  mutate(ID = str_replace(ID, "hMSC_", "hMSC-")) %>%
+  mutate(ID = str_replace(ID, "(?<=0)_(?=[:digit:])", "-"))
 
-# Construct list containing salmon transcript quantification files for each sample
-# folder_tx_quant: root folder containing all quantification results folders
-# dir_tx_quant: list of quantification results folders paths
-# files_tx_quant: list of quantification results files paths
+# Construct transcriptome dataset for limma-voom
 
-folder_tx_quant <- c("./input/cell_quants")
-
-dir_tx_quant <-
-  as.matrix(read.csv(file = "./Input/quant_filenames.csv",
-                     sep = ",",
-                     header = F))
+## Construct this for cDNA data only
 
 files_tx_quant <-
-  file.path(folder_tx_quant, 
-            dir_tx_quant, 
-            "quant.sf")
+  list.files(path = "./input/cDNA",
+             pattern = "*.sf",
+             recursive = TRUE,
+             full.names = T)
 
-names(files_tx_quant) <-
-  as.matrix(read.csv(file = "./input/names.csv", 
-                     sep = ",", 
-                     header = F))
+names_tx_quant <- str_replace(files_tx_quant, pattern = "IonXpress.*$", "") %>% 
+  str_replace(pattern = "./input/cDNA/", "") %>% 
+  str_replace(pattern = "_$", "")
 
-all(file.exists(files_tx_quant)) # Make sure that all transcript files exist
+list_files <- tibble(files = files_tx_quant,
+                         names = names_tx_quant)
+
+all(file.exists(list_files$files)) # Make sure that all transcript files exist
 
 # Import quantification results
 
-matrix_gene_quant <-
-  tximport(
-    files_tx_quant,
-    type = "salmon",
-    tx2gene = tx2gene,
-    ignoreTxVersion = TRUE,
-    countsFromAbundance = "lengthScaledTPM"
-  )  ### NOTE : 4545 transcripts missing ####
+quant_cDNA_tx <-
+  tximeta(
+    list_files,
+    type = "salmon"
+  ) 
 
-names(matrix_gene_quant) # Check format. abundance: TPM, counts: estimated gene counts, length: gene lengths
+# No issues
+
+# Show dataset metadata
+## Imported matrices
+assayNames(quant_cDNA_tx)
+
+## Transcripts imported
+rowRanges(quant_cDNA_tx)
+
+## Genomic information
+seqinfo(quant_cDNA_tx)
+
+## Database information
+retrieveDb(quant_cDNA_tx) %>% class()
+
+## The database originates from ENSEMBL
+
+# Summarise transcripts quantification to genes
+
+quant_cDNA_gene <- summarizeToGene(quant_cDNA_tx,
+                                   countsFromAbundance = "lengthScaledTPM")
+
+# Create object for DGE with edgeR
+## Add sample data
+
+quant_cDNA_DGE <- DGEList(assays(quant_cDNA_gene)[["counts"]])
+
+quant_cDNA_DGE$samples$ID <- rownames(quant_cDNA_DGE$samples)
+  
+quant_cDNA_DGE$samples <- left_join(quant_cDNA_DGE$samples, table_samples)
 
 # Export quantification results
 
-write.csv(matrix_gene_quant, file = "./output/matrix_gene_quantification.csv")
+saveRDS(quant_cDNA_DGE, file = "./output/quant_cDNA_DGE.RDS")
+
+saveRDS(quant_cDNA_tx, file = "./output/quant_cDNA_tx.RDS")
+
+saveRDS(quant_cDNA_gene, file = "./output/quant_cDNA_gene.RDS")
+
+## Construct this for cDNA + ncRNA data from ENSEMBL
+
+files_tx_quant <-
+  list.files(path = "./input/cDNA_ncRNA_ENSEMBL/",
+             pattern = "*.sf",
+             recursive = TRUE,
+             full.names = T)
+
+names_tx_quant <- str_replace(files_tx_quant, pattern = "IonXpress.*$", "") %>% 
+  str_replace(pattern = "./input/cDNA_ncRNA_ENSEMBL/", "") %>% 
+  str_replace(pattern = "_$", "")
+
+list_files <- tibble(files = files_tx_quant,
+                     names = names_tx_quant)
+
+all(file.exists(list_files$files)) # Make sure that all transcript files exist
+
+# Import quantification results
+
+quant_cDNA_ncRNA_ENSEMBL_tx <-
+  tximeta(
+    list_files,
+    type = "salmon"
+  ) 
+
+# No issues
+
+# Show dataset metadata
+## Imported matrices
+assayNames(quant_cDNA_ncRNA_ENSEMBL_tx)
+
+## Transcripts imported
+rowRanges(quant_cDNA_ncRNA_ENSEMBL_tx)
+
+## Genomic information
+seqinfo(quant_cDNA_ncRNA_ENSEMBL_tx)
+
+## Database information
+retrieveDb(quant_cDNA_ncRNA_ENSEMBL_tx) %>% class()
+
+## The database originates from ENSEMBL
+
+# Summarise transcripts quantification to genes
+
+quant_cDNA_ncRNA_ENSEMBL_gene <- summarizeToGene(quant_cDNA_ncRNA_ENSEMBL_tx,
+                                                 countsFromAbundance = "lengthScaledTPM")
+
+# Create object for DGE with edgeR
+## Add sample data
+
+quant_cDNA_ncRNA_ENSEMBL_DGE <- DGEList(assays(quant_cDNA_ncRNA_ENSEMBL_gene)[["counts"]])
+
+quant_cDNA_ncRNA_ENSEMBL_DGE$samples$ID <- rownames(quant_cDNA_ncRNA_ENSEMBL_DGE$samples)
+
+quant_cDNA_ncRNA_ENSEMBL_DGE$samples <- left_join(quant_cDNA_ncRNA_ENSEMBL_DGE$samples, table_samples)
+
+# Export quantification results
+
+saveRDS(quant_cDNA_ncRNA_ENSEMBL_DGE, file = "./output/quant_cDNA_ncRNA_ENSEMBL_DGE.RDS")
+
+saveRDS(quant_cDNA_ncRNA_ENSEMBL_tx, file = "./output/quant_cDNA_ncRNA_ENSEMBL_tx.RDS")
+
+saveRDS(quant_cDNA_ncRNA_ENSEMBL_gene, file = "./output/quant_cDNA_ncRNA_ENSEMBL_gene.RDS")
+
