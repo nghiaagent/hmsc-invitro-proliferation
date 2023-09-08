7106f95a0808971487d6e8f71c97c54fe62b0b41
nghiaagent <nghia6a1@gmail.com>
2023-09-08 16:23:38 +1000

    Import only combined cDNA dataset

--- scripts/dge.R
+++ scripts/dge.R
@@ -1,38 +1,35 @@
 ### Don't source this file by itself; call in from another file after running env_prep.R
 ### Load dataset 
+### Dataset has both mRNA and ncRNA
 
-quant_cDNA_DGE_clean <- readRDS(file = "./output/quant_cDNA_DGE_edgeRfilter.RDS")
-
-quant_cDNA_ncRNA_ENSEMBL_DGE_clean <- readRDS(file = "./output/quant_cDNA_ncRNA_ENSEMBL_DGE_edgeRfilter.RDS")
+quant_DGE_clean <- readRDS(file = "./output/quant_cDNA_ncRNA_ENSEMBL_DGE_edgeRfilter.RDS")
 
 ### Make design matrix 
 
-## Treat cell line as a random effect (duplicateCorrelation)
+## Treat cell line as an additive factor (to use with duplicateCorrelation)
 ## Treat passage and treatment as fixed effects
 ## Treat batch effect as random intercept...?
 
-table_design <- quant_cDNA_DGE_clean$samples %>%
+table_design <- quant_DGE_clean$samples %>%
   mutate(Passage = factor(Passage,
                              levels = c("P5", "P13"))) %>%
   mutate(Treatment = factor(Treatment, 
                                levels = c("Untreated", "Treated")))
 
-design <- model.matrix(~run_date + Passage*Treatment,
+design <- model.matrix(~Passage*Treatment,
                        data = table_design)
 
 ## Apply voom transformation
 
-quant_cDNA_voom <- voom(quant_cDNA_DGE,
+quant_DGE_voom <- voom(quant_DGE_clean,
                             design,
                             plot = TRUE)
 
-quant_cDNA_ncRNA_ENSEMBL_voom <- voom(quant_cDNA_ncRNA_ENSEMBL_DGE,
-                            design,
-                            plot = TRUE)
+## MDS / PCA plot
 
-## MDS plot
+Glimma::glMDSPlot(quant_DGE_clean,
+                  labels = quant_DGE_clean$samples$Sample_ID,
+                  groups = quant_DGE_clean$samples)
 
-plotMDS(quant_cDNA_voom,
-        labels = table_design$run_date)
 
 
