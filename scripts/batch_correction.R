7d516f7423d1298edfe86883cde654f2ba8625bb
Nghia Nguyen <n10304070@qut.edu.au>
2023-09-21 10:24:50 +1000

    gutted this script

--- scripts/batch_correction.R
+++ scripts/batch_correction.R
@@ -2,14 +2,3 @@
 source("./scripts/dge.R")
 
 
-quant_DGE_batchcor_withcovariates <-
-  sva::ComBat_seq(quant_DGE_clean,
-                  batch = quant_DGE_clean$samples$run_date,
-                  covar_mod = design)
-
-quant_DGE_batchcor_nocovariates <- sva::ComBat_seq(quant_DGE_clean$counts,
-                                                   batch = table_design$run_date,
-                                                   group = NULL,
-                                                   full_mod = FALSE)
-quant_DGE_clean_batchcor <- quant_DGE_clean
-quant_DGE_clean_batchcor$counts <- quant_DGE_batchcor_nocovariates
