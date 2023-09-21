48ca4fcff91198efc080a887d7a7e2da8f9de899
Nghia Nguyen <n10304070@qut.edu.au>
2023-09-21 10:27:26 +1000

    added batch correction to code, added code for subsetting full dataset to what I need

--- scripts/dge.R
+++ scripts/dge.R
@@ -12,24 +12,42 @@ quant_DGE_clean <- readRDS(file = "./output/quant_cDNA_ncRNA_ENSEMBL_DGE_edgeRfi
 
 table_design <- quant_DGE_clean$samples %>%
   mutate(Passage = factor(Passage,
-                             levels = c("P5", "P13"))) %>%
+                             levels = c("P5", "P7", "P13"))) %>%
   mutate(Treatment = factor(Treatment, 
                                levels = c("Untreated", "Treated")))
 
-design <- model.matrix(~Passage*Treatment,
+design <- model.matrix(~Passage*Treatment + cell_line,
+                       data = table_design)
+
+# Use ComBat-seq to correct for batch fx
+# Covariates considered: Passage, Treatment, cell population
+
+quant_DGE_batchcor_withcovariates <-
+  sva::ComBat_seq(quant_DGE_clean$counts,
+                  batch = table_design$run_date,
+                  covar_mod = design)
+
+quant_DGE_clean_batchcor <- quant_DGE_clean
+quant_DGE_clean_batchcor$counts <- quant_DGE_batchcor_withcovariates
+
+# Subset data to just P5 vs P13; D3
+
+quant_DGE_clean_batchcor_subset <- quant_DGE_clean_batchcor[,which(
+  quant_DGE_clean_batchcor$samples$Day == "D3" &
+    !quant_DGE_clean_batchcor$samples$Passage == "P7"
+)]
+
+table_design <- quant_DGE_clean_batchcor_subset$samples %>%
+  mutate(Passage = factor(Passage,
+                          levels = c("P5", "P13"))) %>%
+  mutate(Treatment = factor(Treatment, 
+                            levels = c("Untreated", "Treated")))
+
+design <- model.matrix(~Passage*Treatment + cell_line,
                        data = table_design)
 
 ## Apply voom transformation
 
-quant_DGE_voom <- voom(quant_DGE_clean,
+quant_DGE_voom <- voom(quant_DGE_clean_batchcor_subset,
                             design,
                             plot = TRUE)
-
-## MDS / PCA plot
-
-Glimma::glMDSPlot(quant_DGE_clean,
-                  labels = quant_DGE_clean$samples$Sample_ID,
-                  groups = quant_DGE_clean$samples)
-
-
-
