89dff820b08f171a6b3b59090708e571a8403381
nghiaagent <nghia6a1@gmail.com>
2023-09-29 06:13:42 +1000

    gene filtering...

--- scripts/GSEA_passage.R
+++ scripts/GSEA_passage.R
@@ -2,24 +2,37 @@
 
 source("./scripts/dge.R")
 
-# Prepare gene sets
-
-gs_annotations <- EGSEA::buildIdx(entrezIDs = quant_DGE_voom$genes$ENTREZID,
-                species = "human")
 
 # Prepare dataset
 # Swap ENSEMBL ID with ENTREZ ID
+# Remove genes with no Entrez ID
+# Remove genes with duplicated Entrez ID
 
-rownames(quant_DGE_voom$E) <- quant_DGE_voom$genes$ENTREZID
+quant_DGE_EGSEA <- quant_DGE_voom[!is.na(quant_DGE_voom$genes$ENTREZID),]
+quant_DGE_EGSEA <- quant_DGE_EGSEA[!duplicated(quant_DGE_EGSEA$genes$ENTREZID),]
+rownames(quant_DGE_EGSEA$E) <- quant_DGE_EGSEA$genes$ENTREZID
+
+# Prepare gene sets
+
+gs_annotations <- EGSEA::buildIdx(entrezIDs = quant_DGE_EGSEA$genes$ENTREZID,
+                                  species = "human")
 
 # Run EGSEA
 
 egsea_passage <- egsea(
-  voom.results = quant_DGE_voom,
+  voom.results = quant_DGE_EGSEA,
   contrasts = matrix_contrasts_passage,
   gs.annots = gs_annotations,
-  symbolsMap = quant_DGE_voom$genes[,c(2,3)],
-  report.dir = "./outputs/GSEA",
+  symbolsMap = quant_DGE_EGSEA$genes[,c(2,3)],
+  baseGSEAs = c("camera",
+                "roast",
+                "plage",
+                "zscore",
+                "gsva",
+                "ssgsea",
+                "ora",
+                "fry"),
+  report.dir = "./output/GSEA",
   sort.by = "avg.rank",
   report = TRUE
 )
