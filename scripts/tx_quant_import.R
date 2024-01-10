cd83c285af3ace44f5d1dcb68f11664c4407885e
nghiaagent <nghia6a1@gmail.com>
2024-01-10 10:40:38 +1000

    Simplify script for v2 dataset

--- scripts/tx_quant_import.R
+++ scripts/tx_quant_import.R
@@ -8,11 +8,10 @@ table_samples <-
   read_csv("./input/annotation/Cell_sample_table.csv") %>%
   mutate(ID = name,
          .keep = "unused") %>%
-  dplyr::select(!c(name1)) %>%
   mutate(ID = str_replace(ID, "hMSC_", "hMSC-")) %>%
   mutate(ID = str_replace(ID, "(?<=0)_(?=[:digit:])", "-")) %>%
   mutate(cell_line = factor(cell_line,
-                            levels = c("20176", "21558"))) %>%
+                            levels = c("hMSC-20176", "hMSC-21558"))) %>%
   mutate(Passage = factor(Passage,
                           levels = c("P5", "P7", "P13"))) %>%
   mutate(Day = factor(Day,
@@ -21,7 +20,6 @@ table_samples <-
                             levels = c("Untreated", "Treated"))) %>%
   mutate(run_date = str_replace_all(run_date,
                                     "_", "")) %>%
- # mutate(run_date = as.numeric(run_date)) %>%
   mutate(condition_ID = factor(
     str_c(Passage, Day, Treatment, sep = ""),
     levels = c(
@@ -38,7 +36,9 @@ table_samples <-
       "P13D5Untreated",
       "P13D5Treated"
     )
-  ))
+  )) %>%
+  subset(included_in_dataset == TRUE) %>%
+  arrange(ID, cell_line, Passage, Day, Treatment)
 
 # Grab data from ENSEMBL109
 
@@ -46,109 +46,19 @@ ah <- AnnotationHub()
 ensembl109 <- ah[["AH109606"]]
 
 # Construct transcriptome dataset for limma-voom
-
-## Construct this for cDNA data only
-
-files_tx_quant <-
-  list.files(
-    path = "./input/cDNA",
-    pattern = "*.sf",
-    recursive = TRUE,
-    full.names = T
-  )
-
-names_tx_quant <-
-  str_replace(files_tx_quant, pattern = "IonXpress.*$", "") %>%
-  str_replace(pattern = "./input/cDNA/", "") %>%
-  str_replace(pattern = "_$", "")
-
-list_files <- tibble(files = files_tx_quant,
-                     names = names_tx_quant)
-
-all(file.exists(list_files$files)) # Make sure that all transcript files exist
-
-# Import quantification results
-
-quant_cDNA_tx <-
-  tximeta(list_files,
-          type = "salmon")
-
-# No issues
-
-# Show dataset metadata
-## Imported matrices
-assayNames(quant_cDNA_tx)
-
-## Transcripts imported
-rowRanges(quant_cDNA_tx)
-
-## Genomic information
-seqinfo(quant_cDNA_tx)
-
-## Database information
-retrieveDb(quant_cDNA_tx) %>% class()
-
-## The database originates from ENSEMBL
-
-# Summarise transcripts quantification to genes
-
-quant_cDNA_gene <- summarizeToGene(quant_cDNA_tx,
-                                   countsFromAbundance = "lengthScaledTPM")
-
-# Create object for DGE with edgeR
-## Add sample data
-
-quant_cDNA_DGE <- DGEList(assays(quant_cDNA_gene)[["counts"]])
-
-quant_cDNA_DGE$samples$ID <- rownames(quant_cDNA_DGE$samples)
-
-quant_cDNA_DGE$samples <-
-  left_join(quant_cDNA_DGE$samples, table_samples)
-
-## Add annotation data
-
-geneid <- rownames(quant_cDNA_DGE)
-
-genes <-
-  AnnotationDbi::select(
-    ensembl109,
-    keys = geneid,
-    keytype = "GENEID",
-    columns = c(
-      "ENTREZID",
-      "GENENAME",
-      "DESCRIPTION",
-      "GENEBIOTYPE",
-      "SEQNAME"
-    )
-  ) %>%
-  dplyr::distinct(GENEID,
-                  .keep_all = TRUE)
-
-quant_cDNA_DGE$genes <- genes
-
-# Export quantification results
-
-saveRDS(quant_cDNA_DGE, file = "./output/quant_cDNA_DGE.RDS")
-
-saveRDS(quant_cDNA_tx, file = "./output/quant_cDNA_tx.RDS")
-
-saveRDS(quant_cDNA_gene, file = "./output/quant_cDNA_gene.RDS")
-
 ## Construct this for cDNA + ncRNA data from ENSEMBL
 
-files_tx_quant <-
-  list.files(
-    path = "./input/cDNA_ncRNA_ENSEMBL/",
-    pattern = "*.sf",
-    recursive = TRUE,
-    full.names = T
-  )
+files_tx_quant <- table_samples$filename
+
+files_tx_quant <- str_c("./input/cDNA_ncRNA_ENSEMBL/",
+      files_tx_quant,
+      "_quant/quant.sf")
 
 names_tx_quant <-
   str_replace(files_tx_quant, pattern = "IonXpress.*$", "") %>%
   str_replace(pattern = "./input/cDNA_ncRNA_ENSEMBL/", "") %>%
   str_replace(pattern = "_$", "")
+
 
 list_files <- tibble(files = files_tx_quant,
                      names = names_tx_quant)
