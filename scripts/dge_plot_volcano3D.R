18f952aa741ba282fd55f81ff6e2576c7173c84f
nghiaagent <nghia6a1@gmail.com>
2024-02-28 11:38:21 +1000

    change coefficients due to change of contrast matrix in limma script

--- scripts/dge_plot_volcano3D.R
+++ scripts/dge_plot_volcano3D.R
@@ -10,25 +10,25 @@ source("./scripts/dge_cellpops_as_fixed.R")
 ## 4: B vs C (P7 vs P13)
 
 table_design$condition_ID <- factor(table_design$condition_ID,
-          levels = c("P5D3Untreated",
-                     "P7D3Untreated",
-                     "P13D3Untreated"),
-          labels = c("P5",
-                     "P7",
-                     "P13"))
+          levels = c("P5D5Untreated",
+                     "P7D5Untreated",
+                     "P13D5Untreated"),
+          labels = c("A",
+                     "B",
+                     "C"))
 
 polar_pvals <- cbind(
   topTable(fit_contrasts,           number = Inf, sort.by = "none")$P.Value,
-  topTable(fit_contrasts, coef = 4, number = Inf, sort.by = "none")$P.Value,
-  topTable(fit_contrasts, coef = 5, number = Inf, sort.by = "none")$P.Value,
-  topTable(fit_contrasts, coef = 6, number = Inf, sort.by = "none")$P.Value
+  topTable(fit_contrasts, coef = 13, number = Inf, sort.by = "none")$P.Value,
+  topTable(fit_contrasts, coef = 14, number = Inf, sort.by = "none")$P.Value,
+  topTable(fit_contrasts, coef = 15, number = Inf, sort.by = "none")$P.Value
 )
 
 polar_padj <- cbind(
   topTable(fit_contrasts,           number = Inf, sort.by = "none")$adj.P.Val,
-  topTable(fit_contrasts, coef = 4, number = Inf, sort.by = "none")$adj.P.Val,
-  topTable(fit_contrasts, coef = 5, number = Inf, sort.by = "none")$adj.P.Val,
-  topTable(fit_contrasts, coef = 6, number = Inf, sort.by = "none")$adj.P.Val
+  topTable(fit_contrasts, coef = 13, number = Inf, sort.by = "none")$adj.P.Val,
+  topTable(fit_contrasts, coef = 14, number = Inf, sort.by = "none")$adj.P.Val,
+  topTable(fit_contrasts, coef = 15, number = Inf, sort.by = "none")$adj.P.Val
 )
 
 ## Construct volcano3d object
@@ -45,13 +45,8 @@ polar_manual <- polar_coords(
 ## Plot
 
 volcano3D(polar_manual,
-              r_axis_ticks = c(0.5,
-                               1,
-                               1.5,
-                              2,
-                               2.5,
-                               3.0
-                               ),
-              z_axis_ticks = c(0.5),
-              axis_angle = 12/6)
+              axis_angle = 3/6,
+              label_size = 20,
+              axis_label_size = 18,
+          axis_title_size = 28)
 
