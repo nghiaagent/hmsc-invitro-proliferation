0656d2a09ef09940c036fd1bb3decf3547f9fa07
nghiaagent <nghia6a1@gmail.com>
2024-05-30 11:23:14 +1000

    Rename files for clarity

---
+++ scripts/0_meta_env_prep.R
@@ -0,0 +1,101 @@
+# Install pacman, a package manager to install and load multiple packages at once
+
+if (!requireNamespace("pacman")) {
+  install.packages("pacman")
+}
+library(pacman)
+
+# Install Bioconductor package manager
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+library(BiocManager)
+
+# List Bioconductor packages to load/install
+
+list_Bioc_Pkg <- c(
+  "BiocHubsShiny",
+  "AnnotationHub",
+  "edgeR",
+  "Homo.sapiens",
+  "limma",
+  "Glimma",
+  "EnsDb.Hsapiens.v75",
+  "ComplexHeatmap",
+  "tximeta",
+  "SummarizedExperiment",
+  "PCAtools",
+  "sva",
+  "ensembldb",
+  "GSVA",
+  "AnnotationDbi",
+  "topGO",
+  "pathview",
+  "gage",
+  "globaltest",
+  "safe",
+  "org.Hs.eg.db",
+  "clusterProfiler",
+  "ReactomePA",
+  "reactome.db",
+  "enrichplot",
+  "GSEABase",
+  "BiocStyle",
+  "CEMiTool",
+  "HDO.db"
+)
+
+# Install Bioconductor packages, if they are not yet installed
+# Swap update = TRUE / FALSE depending on need to update pkg
+
+BiocManager::install(
+  pkgs = list_Bioc_Pkg,
+  update = FALSE
+)
+
+# Load Bioconductor packages
+
+invisible(lapply(list_Bioc_Pkg, function(x)
+  library(x, character.only = TRUE)))
+
+# Load all CRAN packages
+
+p_load(
+  tibble,
+  tidyverse,
+  ggrepel,
+  ggpubr,
+  rstatix,
+  ggalt,
+  ggplotify,
+  cowplot,
+  gridExtra,
+  statmod,
+  volcano3D,
+  writexl,
+  msigdbr,
+  cowplot,
+  viridis,
+  palettetown,
+  seriation,
+  circlize,
+  magick,
+  cluster,
+  rmdformats,
+  future,
+  shinybusy,
+  WGCNA
+)
+
+# Clean up package list
+
+rm(list_Bioc_Pkg)
+
+# Limit number of cores used due to memory issues on laptops.
+
+BiocParallel::register(SnowParam(workers = 8),
+                       default = T)
+bpparam()
+
+# TO ADD: Create folders needed for outputting files
---
+++ scripts/1_pre_quant_import.R
@@ -0,0 +1,164 @@
+### Don't source this file by itself; call in from another file after running env_prep.R
+### Source
+### Import salmon transcript quantification results to a single matrix, export that matrix.
+
+# Construct sample table
+
+table_samples <-
+  read_csv("./input/annotation/Cell_sample_table.csv") %>%
+  mutate(ID = name,
+         .keep = "unused") %>%
+  mutate(ID = str_replace(ID, "hMSC_", "hMSC-")) %>%
+  mutate(ID = str_replace(ID, "(?<=0)_(?=[:digit:])", "-")) %>%
+  mutate(cell_line = factor(cell_line,
+                            levels = c("hMSC-20176", "hMSC-21558"))) %>%
+  mutate(Passage = factor(Passage,
+                          levels = c("P5", "P7", "P13"))) %>%
+  mutate(Day = factor(Day,
+                      levels = c("D3", "D5"))) %>%
+  mutate(Treatment = factor(Treatment,
+                            levels = c("Untreated", "Treated"))) %>%
+  mutate(run_date = str_replace_all(run_date,
+                                    "_", "")) %>%
+  mutate(timepoint_ID = factor(
+    str_c(Passage, Day, sep = ""),
+    levels = c(
+      "P5D3",
+      "P5D5",
+      "P7D3",
+      "P7D5",
+      "P13D3",
+      "P13D5"
+    )
+  )) %>%
+  mutate(condition_ID = factor(
+    str_c(Passage, Day, Treatment, sep = ""),
+    levels = c(
+      "P5D3Untreated",
+      "P5D3Treated",
+      "P5D5Untreated",
+      "P5D5Treated",
+      "P7D3Untreated",
+      "P7D3Treated",
+      "P7D5Untreated",
+      "P7D5Treated",
+      "P13D3Untreated",
+      "P13D3Treated",
+      "P13D5Untreated",
+      "P13D5Treated"
+    )
+  )) %>%
+  subset(included_in_dataset == TRUE) %>%
+  arrange(ID, cell_line, Passage, Day, Treatment) %>%
+  mutate(ID = factor(ID)) %>%
+  mutate(ID = fct_inorder(ID))
+
+# Grab data from ENSEMBL109
+
+ah <- AnnotationHub()
+ensembl109 <- ah[["AH109606"]]
+
+# Construct transcriptome dataset for limma-voom
+## Construct this for cDNA + ncRNA data from ENSEMBL
+
+files_tx_quant <- table_samples$filename
+
+files_tx_quant <- str_c("./input/cDNA_ncRNA_ENSEMBL/",
+      files_tx_quant,
+      "_quant/quant.sf")
+
+names_tx_quant <-
+  str_replace(files_tx_quant, pattern = "IonXpress.*$", "") %>%
+  str_replace(pattern = "./input/cDNA_ncRNA_ENSEMBL/", "") %>%
+  str_replace(pattern = "_$", "")
+
+
+list_files <- tibble(files = files_tx_quant,
+                     names = names_tx_quant)
+
+all(file.exists(list_files$files)) # Make sure that all transcript files exist
+
+# Import quantification results
+
+quant_cDNA_ncRNA_ENSEMBL_tx <-
+  tximeta(list_files,
+          type = "salmon")
+
+# No issues
+
+# Show dataset metadata
+## Imported matrices
+assayNames(quant_cDNA_ncRNA_ENSEMBL_tx)
+
+## Transcripts imported
+rowRanges(quant_cDNA_ncRNA_ENSEMBL_tx)
+
+## Genomic information
+seqinfo(quant_cDNA_ncRNA_ENSEMBL_tx)
+
+## Database information
+retrieveDb(quant_cDNA_ncRNA_ENSEMBL_tx) %>% class()
+
+## The database originates from ENSEMBL
+
+# Summarise transcripts quantification to genes
+
+quant_cDNA_ncRNA_ENSEMBL_gene <-
+  summarizeToGene(quant_cDNA_ncRNA_ENSEMBL_tx,
+                  countsFromAbundance = "lengthScaledTPM")
+
+# Create object for DGE with edgeR
+## Add sample data
+
+quant_cDNA_ncRNA_ENSEMBL_DGE <-
+  DGEList(assays(quant_cDNA_ncRNA_ENSEMBL_gene)[["counts"]])
+
+quant_cDNA_ncRNA_ENSEMBL_DGE$samples <-
+  cbind(quant_cDNA_ncRNA_ENSEMBL_DGE$samples, table_samples)
+
+
+## Add annotation data
+
+geneid <- rownames(quant_cDNA_ncRNA_ENSEMBL_DGE)
+
+genes <-
+  AnnotationDbi::select(
+    ensembl109,
+    keys = geneid,
+    keytype = "GENEID",
+    columns = c(
+      "ENTREZID",
+      "GENENAME",
+      "DESCRIPTION",
+      "GENEBIOTYPE",
+      "SEQNAME"
+    )
+  ) %>%
+  dplyr::distinct(GENEID,
+                  .keep_all = TRUE)
+
+### Add inclusion of genes in MSigDB h
+
+msigdb_h  <-
+  read.gmt("./input/genesets/msigdb_v2023.2.Hs_GMTs/h.all.v2023.2.Hs.entrez.gmt") %>%
+  mutate(term = as.character(term)) %>%
+  mutate(ENTREZID = as.integer(gene)) %>%
+  mutate(gene = NULL) %>%
+  nest_by(ENTREZID) %>%
+  rowwise() %>% 
+  mutate(data = list(deframe(data))) %>%
+  mutate(data = str_flatten_comma(data)) %>%
+  mutate(msigdb_h = data) %>%
+  mutate(data = NULL)
+
+genes <- left_join(genes, msigdb_h)
+
+quant_cDNA_ncRNA_ENSEMBL_DGE$genes <- genes
+
+# Export quantification results
+
+saveRDS(quant_cDNA_ncRNA_ENSEMBL_DGE, file = "./output/quant_cDNA_ncRNA_ENSEMBL_DGE.RDS")
+
+saveRDS(quant_cDNA_ncRNA_ENSEMBL_tx, file = "./output/quant_cDNA_ncRNA_ENSEMBL_tx.RDS")
+
+saveRDS(quant_cDNA_ncRNA_ENSEMBL_gene, file = "./output/quant_cDNA_ncRNA_ENSEMBL_gene.RDS")
