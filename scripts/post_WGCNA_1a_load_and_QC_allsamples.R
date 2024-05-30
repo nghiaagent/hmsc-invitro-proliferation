df23285d90c9d10783a4fb679bcf048ba488e4b0
nghiaagent <nghia6a1@gmail.com>
2024-05-30 11:16:41 +1000

    First commit

---
+++ scripts/post_WGCNA_1a_load_and_QC_allsamples.R
@@ -0,0 +1,187 @@
+#### Run after DGE pipeline
+
+# Transpose logCPM matrix for WGCNA
+# Change from ENSEMBL to ENTREZ (to help with GSVA)
+
+x <- quant_DGE_voom
+
+rownames(x$genes) <- x$genes$GENEID
+
+order <- order(fit$Amean, decreasing = TRUE)
+
+x <- x[order,]
+
+x <- x[complete.cases(dplyr::select(x$genes,!msigdb_h)),]
+
+x <- x[!duplicated(x$genes$ENTREZID),]
+
+rownames(x$E) <- x$genes$ENTREZID
+
+rownames(x$genes) <- x$genes$ENTREZID
+
+x <- x[order(x$genes$GENEID, decreasing = FALSE),]
+
+x$genes <- x$genes %>% dplyr::select(!GENEID)
+
+quant_DGE_WGCNA <- x
+
+rm(x)
+
+GCN_E <- t(quant_DGE_WGCNA$E) %>%
+  as.data.frame()
+
+# Check for low quality genes and samples
+## Should be all OK
+## If not, remove offending genes and samples from data
+
+GCN_qual <- goodSamplesGenes(GCN_E)
+
+if (!GCN_qual$allOK)
+{
+  # Print  genes and samples that are removed:
+  if (sum(!GCN_qual$goodGenes) > 0)
+    printFlush(paste("Removing genes:",
+                     paste(names(datExpr0)[!GCN_qual$goodGenes],
+                           collapse = ", ")))
+  
+  
+  if (sum(!GCN_qual$goodSamples) > 0)
+    printFlush(paste("Removing samples:",
+                     paste(rownames(datExpr0)[!GCN_qual$goodSamples],
+                           collapse = ", ")))
+  
+  
+  # Remove the offending genes and samples from the data:
+  GCN_E <- GCN_E[GCN_qual$goodSamples, GCN_qual$goodGenes]
+}
+
+# Perform sample clustering to detect outliers
+## No outliers that are not already known (i.e. all related to batch effect)
+
+meta_exclude <- c(
+  'group',
+  'norm.factors',
+  'filename',
+  'Sample_ID',
+  'included_in_dataset',
+  "lib.size",
+  "ID",
+  "timepoint_ID",
+  "condition_ID"
+)
+png(
+  "./output/plots_WGCNA/WGCNA_allsamples/Sample clustering.png",
+  res = 150,
+  width = 40,
+  height = 25,
+  units = "cm"
+)
+
+plotDendroAndColors(
+  dendro = hclust(dist(GCN_E),
+                  method = "average"),
+  colors = labels2colors(dplyr::select(
+    quant_DGE_WGCNA$targets,!all_of(meta_exclude)
+  )),
+  groupLabels = names(dplyr::select(
+    quant_DGE_WGCNA$targets,!all_of(meta_exclude)
+  )),
+  main = "Hierarchical clustering of samples"
+)
+
+dev.off()
+
+# Select soft thresholding power
+
+## Calculate scale-free topology fit index
+
+powers <- c(c(1:10), seq(from = 12, to = 20, by = 2))
+
+GCN_sft <- pickSoftThreshold(
+  GCN_E,
+  powerVector = powers,
+  verbose = 5
+)
+
+## Plot scale-free topology fit index and mean connectivity
+
+png(
+  "./output/plots_WGCNA/WGCNA_allsamples/Soft thresholding index selection.png",
+  res = 150,
+  width = 40,
+  height = 20,
+  units = "cm"
+)
+
+par(mfrow = c(1, 2))
+cex1 = 0.9
+
+### SFT index
+
+plot(
+  GCN_sft$fitIndices[, 1],-sign(GCN_sft$fitIndices[, 3]) * GCN_sft$fitIndices[, 2],
+  xlab = "Soft Threshold (power)",
+  ylab = "Scale Free Topology Model Fit,signed R^2",
+  type = "n",
+  main = paste("Scale independence")
+)
+
+text(
+  GCN_sft$fitIndices[, 1],-sign(GCN_sft$fitIndices[, 3]) * GCN_sft$fitIndices[, 2],
+  labels = powers,
+  cex = cex1,
+  col = "red"
+)
+
+abline(h = 0.90, col = "red")
+
+### Mean connectivity
+
+plot(
+  GCN_sft$fitIndices[, 1],
+  GCN_sft$fitIndices[, 5],
+  xlab = "Soft Threshold (power)",
+  ylab = "Mean Connectivity",
+  type = "n",
+  main = paste("Mean connectivity")
+)
+text(
+  GCN_sft$fitIndices[, 1],
+  GCN_sft$fitIndices[, 5],
+  labels = powers,
+  cex = cex1,
+  col = "red"
+)
+
+dev.off()
+
+# I agree with the power selected in the object (power 7)
+
+# Construct GCN object
+
+GCN <- list(
+  E = GCN_E,
+  targets = quant_DGE_WGCNA$targets %>%
+    mutate(
+      cell_line = as.numeric(cell_line),
+      Passage = as.numeric(Passage),
+      Day = as.numeric(Day),
+      Treatment = as.numeric(Treatment),
+      run_date = as.numeric(factor(run_date)),
+      timepoint_ID = as.numeric(timepoint_ID)
+    ) %>%
+    select(
+      lib.size,
+      cell_line,
+      Passage,
+      Day,
+      Treatment,
+      run_date,
+      timepoint_ID
+    ),
+  genes = quant_DGE_voom$genes,
+  sft = GCN_sft
+)
+
+save(GCN,
+     file = "./output/data_WGCNA/WGCNA_allsamples/GCN_input.Rdata")
---
+++ scripts/post_WGCNA_2b_construct_net_D3_UT_only.R
@@ -0,0 +1,97 @@
+# Load object
+
+load(file = "./output/data_WGCNA/WGCNA_D3_UT/GCN_input.Rdata")
+
+# Construct GCN
+
+GCN$net <- blockwiseModules(datExpr = GCN$E,
+                        power = GCN$sft$powerEstimate,
+                        TOMType = "unsigned",
+                        minModuleSize = 30,
+                        reassignThreshold = 0,
+                        mergeCutHeight = 0.25,
+                        numericLabels = TRUE,
+                        pamRespectsDendro = FALSE,
+                        maxBlockSize = Inf,
+                        saveTOMs = TRUE,
+                        verbose = 5)
+
+# Plot dendrogram
+
+png(
+  "./output/plots_WGCNA/WGCNA_D3_UT/GCN dendrogram.png",
+  res = 150,
+  width = 40,
+  height = 20,
+  units = "cm"
+)
+
+plotDendroAndColors(
+  GCN$net$dendrograms[[1]],
+  labels2colors(GCN$net$colors)[GCN$net$blockGenes[[1]]],
+  "Module colors",
+  dendroLabels = FALSE,
+  hang = 0.03,
+  addGuide = TRUE,
+  guideHang = 0.05
+)
+
+dev.off()
+
+# Correlate modules to samples metadata
+
+MEs_plot <- moduleEigengenes(GCN$E,
+                             labels2colors(GCN$net$colors))$eigengenes %>%
+  orderMEs()
+
+## Calculate pairwise Pearson cor of MEs against sample metadata
+
+module2trait <- list(
+  cor = cor(MEs_plot,
+            GCN$targets,
+            use = "p"),
+  pvalue = cor(MEs_plot,
+               GCN$targets,
+               use = "p") %>%
+    corPvalueStudent(nrow(GCN$E))
+)
+
+# Will display correlations and their p-values
+
+png(
+  "./output/plots_WGCNA/WGCNA_D3_UT/Correlation of module PC1s to variables.png",
+  res = 150,
+  width = 30,
+  height = 30,
+  units = "cm"
+)
+
+textMatrix =  paste(signif(module2trait$cor, 2),
+                    "\n(",
+                    signif(module2trait$pvalue, 1),
+                    ")",
+                    sep = "")
+
+dim(textMatrix) = dim(module2trait$cor)
+par(mar = c(6, 8.5, 3, 3),
+    cex = 1.3)
+
+# Display the correlation values within a heatmap plot
+
+labeledHeatmap(
+  Matrix = module2trait$cor,
+  xLabels = names(GCN$targets),
+  yLabels = names(MEs_plot),
+  ySymbols = names(MEs_plot),
+  colorLabels = FALSE,
+  colors = blueWhiteRed(50),
+  textMatrix = textMatrix,
+  setStdMargins = FALSE,
+  cex.text = 0.5,
+  zlim = c(-1, 1),
+  main = paste("Module-variable relationships")
+)
+
+dev.off()
+
+save(GCN, file = "./output/data_WGCNA/WGCNA_D3_UT/GCN_output.RData")
