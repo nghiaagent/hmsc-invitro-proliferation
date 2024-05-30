45baa0eacbbc8be34550062e8d70e93c606662a3
nghiaagent <nghia6a1@gmail.com>
2024-05-31 09:45:12 +1000

    Update file names

--- scripts/9_pipeline_DGE.R
+++ scripts/9_pipeline_DGE.R
@@ -1,5 +1,9 @@
 # Run env_prep and dge_cellpops_as_fixed in succession to load packages and run DGE
 
-source("./scripts/env_prep.R")
+source(file.path(".",
+                 "scripts",
+                 "0_meta_env_prep.R"))
 
-source("./scripts/dge_cellpops_as_fixed.R")
+source(file.path(".",
+                 "scripts",
+                 "2_dge.R"))
--- scripts/9_pipeline_WGCNA.R
+++ scripts/9_pipeline_WGCNA.R
@@ -2,28 +2,28 @@
 
 source(file.path(".",
                  "scripts",
-                 "env_prep.R"))
+                 "0_meta_env_prep.R"))
 
 source(file.path(".",
                  "scripts",
-                 "dge_cellpops_as_fixed.R"))
+                 "2_dge.R"))
 
 # Perform WGCNA with all samples
 
 source(file.path(".",
                  "scripts",
-                 "post_WGCNA_1a_load_and_QC_allsamples.R"))
+                 "5_post_WGCNA_1a_load_and_QC_allsamples.R"))
 
 source(file.path(".",
                  "scripts",
-                 "post_WGCNA_2a_construct_net_allsamples.R"))
+                 "5_post_WGCNA_2a_construct_net_allsamples.R"))
 
 # Perform WGCNA on D3 UT samples only
 
 source(file.path(".",
                  "scripts",
-                 "post_WGCNA_1b_load_and_QC_D3_UT_only.R"))
+                 "5_post_WGCNA_1b_load_and_QC_D3_UT_only.R"))
 
 source(file.path(".",
                  "scripts",
-                 "post_WGCNA_2b_construct_net_D3_UT_only.R"))
+                 "5_post_WGCNA_2b_construct_net_D3_UT_only.R"))
