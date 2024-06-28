14ee60c15e7acb0f3a3777251203e21f43960fd5
nghiaagent <nghia6a1@gmail.com>
2024-06-28 13:08:26 +1000

    Remove CEMiTool use, export gene lists to GMTs

--- scripts/5_post_WGCNA_extract_module_genes.R
+++ scripts/5_post_WGCNA_extract_module_genes.R
@@ -10,89 +10,42 @@ load(file.path(
   'GCN_output.Rdata'
 ))
 
-GCN_allsamples <- GCN
-
-## GCN with only D3 UT samples
-
-load(file.path('.',
-               'output',
-               'data_WGCNA',
-               'WGCNA_D3_UT',
-               'GCN_output.Rdata'))
-
-GCN_subset <- GCN
-
-rm(GCN)
-
 ## Add module assignment to gene list
 
-GCN_subset$genes$module <- GCN_subset$net$colors %>%
+GCN$genes$module <- GCN$net$colors %>%
   labels2colors()
 
-GCN_subset$genes <- GCN_subset$genes %>%
-  mutate(ENTREZID = as.character(ENTREZID))
-
-GCN_allsamples$genes$module <- GCN_allsamples$net$colors %>%
-  labels2colors()
-
-GCN_allsamples$genes <- GCN_allsamples$genes %>%
+GCN$genes <- GCN$genes %>%
   mutate(ENTREZID = as.character(ENTREZID))
 
 ## Extract gene lists
+## ENTREZ for GSVA
+## GENENAME for String/PPI
 
-### subset
-
-GCN_subset_genelists <- split(GCN_subset$genes,
-                              GCN_subset$genes$module,
-                              drop = TRUE) %>%
+GCN_genelists_ENTREZ <- split(GCN$genes, GCN$genes$module, drop = TRUE) %>%
   lapply(function(x) {
     select(x, ENTREZID) %>%
       as_vector %>%
       unname()
   })
 
-### All data
-
-GCN_allsamples_genelists <- split(GCN_allsamples$genes,
-                                  GCN_allsamples$genes$module,
-                                  drop = TRUE) %>%
+GCN_genelists_GENENAME <- split(GCN$genes, GCN$genes$module, drop = TRUE) %>%
   lapply(function(x) {
-    select(x, ENTREZID) %>%
+    select(x, GENENAME) %>%
       as_vector %>%
       unname()
   })
 
-GCN_genelists_WGCNA <-
-  c(GCN_allsamples_genelists, GCN_subset_genelists)
+names(GCN_genelists_ENTREZ)   <- str_c("WGCNA_allsamples_", names(GCN_genelists_ENTREZ))
+names(GCN_genelists_GENENAME) <- str_c("WGCNA_allsamples_", names(GCN_genelists_GENENAME))
 
-names(GCN_genelists_WGCNA) <- c(str_c("WGCNA_allsamples_",
-                                      names(GCN_allsamples_genelists)),
-                                str_c("WGCNA_subset_",
-                                      names(GCN_subset_genelists)))
+# Define function to write list to GMT file
+### Function by Levi Waldron.
+### R list object that will be converted to GMT file.  Each element
+### should contain a vector of gene names, and the names of the
+### elements will used for the gene set names
+### Output file name for .gmt file
 
-## Combine with CEMiTool files to make GCN gene lists
-
-GCN_genelists_CEMi <- readGMT(file.path(".",
-                                        "output",
-                                        "data_WGCNA",
-                                        "CEMiTool",
-                                        "modules_genes.gmt"))
-
-names(GCN_genelists_CEMi) <- c(str_c("CEMiTool_",
-                                     names(GCN_genelists_CEMi)))
-
-GCN_genelists <- c(GCN_genelists_WGCNA, GCN_genelists_CEMi)
-  
-  # Define function to write list to GMT file
-  #Create a gmt (gene matrix transposed) file
-  ### Createss a gmt (gene matrix transposed) file such as those
-  ### provided by mSigDB or geneSigDB, from an R list object.
-  ### Function by Levi Waldron.
-  ### R list object that will be converted to GMT file.  Each element
-  ### should contain a vector of gene names, and the names of the
-  ### elements will used for the gene set names
-  ### Output file name for .gmt file
-  
 writeGMT <- function (object, fname) {
   if (class(object) != "list")
     stop("object should be of class 'list'")
@@ -115,14 +68,40 @@ writeGMT <- function (object, fname) {
   }
 }
 
-writeGMT(GCN_genelists,
-         file.path(".",
-                   "input",
-                   "genesets",
-                   "GCN_sets.gmt"))
+# Write gene list in ENTREZID to GMT file
 
-saveRDS(GCN_genelists,
-        file = file.path('.',
-                         'output',
-                         'data_WGCNA',
-                         'GCN_genelists.RDS'))
+writeGMT(
+  GCN_genelists_ENTREZ,
+  file.path(
+    ".",
+    "input",
+    "genesets",
+    "GCN_sets_WGCNA_allsamples_only.gmt"
+  )
+)
+
+# Write gene list with GENENAME to text files for STRING
+
+fwrite(
+  GCN_genelists_GENENAME[["WGCNA_allsamples_turquoise"]] %>% list(),
+  file = file.path(
+    ".",
+    "output",
+    "data_WGCNA",
+    "WGCNA_allsamples",
+    "turquoise_genes.txt"
+  ),
+  na = ""
+)
+
+fwrite(
+  GCN_genelists_GENENAME[["WGCNA_allsamples_red"]] %>% list(),
+  file = file.path(
+    ".",
+    "output",
+    "data_WGCNA",
+    "WGCNA_allsamples",
+    "red_genes.txt"
+  ),
+  na = ""
+)
