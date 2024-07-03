19f36104a60282ca0721e918684576e2712335fa
nghiaagent <nghia6a1@gmail.com>
2024-07-04 01:27:55 +1000

    Remove unneccessary code

--- scripts/0_meta_define_GOIs.R
+++ scripts/0_meta_define_GOIs.R
@@ -33,62 +33,10 @@ genenames_inhouse <- c(
   "POU5F1"
 )
 
-genenames_rt2array <- c(
-  "A2M",
-  "ABCA1",
-  "ACHE",
-  "ADAM10",
-  "ADAM9",
-  "APBA1",
-  "APBA3",
-  "APBB1",
-  "APBB2",
-  "APH1A",
-  "APLP1",
-  "APLP2",
-  "APOA1",
-  "APOE",
-  "APP",
-  "BACE1",
-  "BACE2",
-  "BCHE",
-  "BDNF",
-  "CAPN1",
-  "CASP3",
-  "CASP4",
-  "CDK1",
-  "CDK5",
-  "CDKL1",
-  "CHAT",
-  "CLU",
-  "CTSB",
-  "CTSC",
-  "CTSD",
-  "CTSG",
-  "CTSL",
-  "EP300",
-  "ERN1",
-  "GAP43",
-  "GNAO1",
-  "GNAZ",
-  "GNB1",
-  "GNB2",
-  "GNB4",
-  "GNB5",
-  "GNG11",
-  "GNG3",
-  "GNG4"
-)
-
 geneIds_inhouse <- mapIds(org.Hs.eg.db,
                           keys = genenames_inhouse,
                           column = "ENTREZID",
                           keytype = "SYMBOL")
-
-geneIds_rt2array <- mapIds(org.Hs.eg.db,
-                           keys = genenames_rt2array,
-                           column = "ENTREZID",
-                           keytype = "SYMBOL")
 
 ## Define genes in WNT signalling
 
@@ -106,17 +54,3 @@ names(geneIds_Wnt) <- mapIds(org.Hs.eg.db,
                              column = "SYMBOL",
                              keytype = "ENTREZID")
 
-## Define genes related to HSPGs (not already in in-house set)
-
-msigdb_KEGG <- getGmt(con = "./input/genesets/msigdb_v2023.2.Hs_GMTs/c2.cp.kegg_legacy.v2023.2.Hs.entrez.gmt")
-
-geneIds_HSPGs <- c(msigdb_KEGG[["KEGG_GLYCOSAMINOGLYCAN_BIOSYNTHESIS_HEPARAN_SULFATE"]]@geneIds,
-                   msigdb_GOBP[["GOBP_HEPARAN_SULFATE_PROTEOGLYCAN_BIOSYNTHETIC_PROCESS"]]@geneIds,
-                   msigdb_GOMF[["GOMF_HEPARAN_SULFATE_PROTEOGLYCAN_BINDING"]]@geneIds) %>%
-  unique() %>%
-  setdiff(geneIds_inhouse)
-
-names(geneIds_HSPGs) <- mapIds(org.Hs.eg.db,
-                               keys = geneIds_HSPGs,
-                               column = "SYMBOL",
-                               keytype = "ENTREZID")
