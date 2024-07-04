e72bd3bd25476b4a3f487a289588c7822d12eefa
nghiaagent <nghia6a1@gmail.com>
2024-07-05 01:10:06 +1000

    Swap package

--- scripts/3_dge_extract_genes_excel.R
+++ scripts/3_dge_extract_genes_excel.R
@@ -1,14 +1,13 @@
-# Prepare dataset.
-
 fit_contrasts <-
   readRDS(file = "./output/data_expression/post_DGE/fit_contrasts.RDS")
 
 list_top <- map(1:ncol(fit_contrasts$coefficients),
-         \ (x) topTable(fit_contrasts, coef = x, n = Inf))
+                \ (x) topTable(fit_contrasts, coef = x, n = Inf))
 
 names(list_top) <- colnames(fit_contrasts$contrasts)
 
-writexl::write_xlsx(list_top, path = file.path("output",
-                                               "data_expression",
-                                               "post_DGE",
-                                               "genes_all.xlsx"))
+openxlsx::write.xlsx(
+  list_top,
+  file = file.path("output", "data_expression", "post_DGE", "genes_all.xlsx"),
+  asTable = TRUE,
+  tableStyle = "TableStyleMedium2")
