81b9d1537359d7b33185622387fdce30b1241ec0
nghiaagent <nghia6a1@gmail.com>
2024-07-05 00:30:57 +1000

    Change annotation, add alternate heatmap

--- scripts/1_pre_expressionheatmap.R
+++ scripts/1_pre_expressionheatmap.R
@@ -4,14 +4,13 @@
 # limma authors recommend using logCPM from cpm() or voom()
 # https://www.biostars.org/p/9511346/
 
-source("./scripts/dge_cellpops_as_fixed.R")
-
 siggenes <- topTable(fit_contrasts, number = Inf, sort.by = "F") %>%
   filter(adj.P.Val < 0.05)
 
-quant_heatmap <- quant_DGE_voom[siggenes$GENEID, ]
+fit_heatmap <- fit_contrasts[siggenes$ENSEMBL, ]
+fit_heatmap$EList <- fit_heatmap$EList[siggenes$ENSEMBL, ]
 
-E_heatmap <- t(scale(t(quant_heatmap$E)))
+E_heatmap <- t(scale(t(fit_heatmap$EList$E)))
 
 # Set color scheme and breaks
 
@@ -22,35 +21,31 @@ breaks <- seq(-2, 2, length.out = 100)
 
 ## For annotation data
 
-col_cell_line <- palettetown::pokepal(6)[c(3, 4)]
+col_treatment <- palettetown::pokepal(283)[c(11, 5, 7, 8)]
 
-col_treatment <- palettetown::pokepal(283)[c(11, 5)]
-
-col_passage <- palettetown::pokepal(191)[c(8, 3, 9)]
-
-col_day <- palettetown::pokepal(283)[c(3, 1)]
+col_genotype <- palettetown::pokepal(283)[c(3, 1)]
 
 # Build annotation; include only necessary metadata
 
 ## Dataframe of annotation data
 
 anno <- tibble(
-  `Cell population` = quant_heatmap$targets$cell_line,
-  `Passage` = quant_heatmap$targets$Passage,
-  `Day` = quant_heatmap$targets$Day,
-  `Treatment group` = quant_heatmap$targets$Treatment
+  `Genotype` = fit_heatmap$targets$genotype,
+  `Treatment` = fit_heatmap$targets$treatment_combined
 )
 
 ## Colour mapping
 
 anno_cols <- list(
-  `Cell population` = c(
-    'hMSC-20176' = col_cell_line[1],
-    'hMSC-21558' = col_cell_line[2]
+  `Genotype` = c(
+    'WT' = col_genotype[1],
+    'ATP7B-KO' = col_genotype[2]
   ),
-  `Passage` = c('P5' = col_passage[1], 'P7' = col_passage[2], 'P13' = col_passage[3]),
-  `Day` = c('D3' = col_day[1], 'D5' = col_day[2]),
-  `Treatment` = c('Untreated' = col_treatment[1], 'Treated' = col_treatment[2])
+  `Treatment` = c('Untreated' = col_treatment[1],
+                  'Cu' = col_treatment[2],
+                  'Cu_D-penicilamine' = col_treatment[3],
+                  'Cu_trientine' = col_treatment[4]
+                  )
 )
 
 ## ComplexHeatmap metadata annotation object
@@ -61,32 +56,16 @@ anno_object <- HeatmapAnnotation(
   col = anno_cols,
   annotation_height = 0.6,
   annotation_legend_param = list(
-    `Cell population` = list(
+    `Genotype` = list(
       nrow = 2,
-      title = 'Cell population',
+      title = 'Genotype',
       title_position = 'topleft',
       legend_direction = 'vertical',
       title_gp = gpar(fontsize = 12, fontface = 'bold'),
       labels_gp = gpar(fontsize = 12, fontface = 'bold')
     ),
-    `Passage` = list(
-      nrow = 3,
-      title = 'Passage',
-      title_position = 'topleft',
-      legend_direction = 'vertical',
-      title_gp = gpar(fontsize = 12, fontface = 'bold'),
-      labels_gp = gpar(fontsize = 12, fontface = 'bold')
-    ),
-    `Day` = list(
-      nrow = 2,
-      title = 'Day',
-      title_position = 'topleft',
-      legend_direction = 'vertical',
-      title_gp = gpar(fontsize = 12, fontface = 'bold'),
-      labels_gp = gpar(fontsize = 12, fontface = 'bold')
-    ),
-    `Treatment group` = list(
-      nrow = 2,
+    `Treatment` = list(
+      nrow = 4,
       title = 'Treatment',
       title_position = 'topleft',
       legend_direction = 'vertical',
@@ -132,7 +111,9 @@ clusters$clustering <- factor(clusters$clustering,
 
 # Create heatmap object
 
-heatmap <- Heatmap(
+## Unclustered
+
+heatmap_unclustered <- Heatmap(
   E_heatmap,
   name = 'Gene\nZ-\nscore',
   col = colorRamp2(breaks, col),
@@ -168,7 +149,69 @@ heatmap <- Heatmap(
   # column (sample) parameters
 
   cluster_column_slices = F,
-  column_split = quant_heatmap$targets$Sample_ID,
+  cluster_columns = F,
+  show_column_dend = T,
+  show_column_names = F,
+  
+  # specify top and bottom annotations
+  
+  top_annotation = anno_object
+)
+
+# Export heatmap
+
+png(
+  file = "./output/plots_heatmap/normalised_expression_genes_unclustered.png",
+  width = 12,
+  height = 12,
+  units = "in",
+  res = 1200
+)
+
+export_heatmap <- plot(
+  heatmap_unclustered
+)
+
+dev.off()
+
+## Clustered
+
+heatmap_clustered <- Heatmap(
+  E_heatmap,
+  name = 'Gene\nZ-\nscore',
+  col = colorRamp2(breaks, col),
+  border = F,
+  
+  # Group rows based on clusters
+  
+  row_split = clusters$clustering,
+  cluster_row_slices = FALSE,
+  
+  # parameters for the colour-bar that represents gradient of expression
+  
+  heatmap_legend_param = list(
+    color_bar = 'continuous',
+    legend_direction = 'vertical',
+    legend_width = unit(8, 'cm'),
+    legend_height = unit(5.0, 'cm'),
+    title_position = 'topcenter',
+    title_gp = gpar(fontsize = 12, fontface = 'bold'),
+    labels_gp = gpar(fontsize = 12, fontface = 'bold')
+  ),
+  
+  # row (gene) parameters
+  
+  cluster_rows = T,
+  show_row_dend = T,      
+  row_title = levels(clusters$clustering),
+  row_title_side = 'left',
+  row_title_gp = gpar(fontsize = 10,  fontface = 'bold'),
+  row_title_rot = 90,
+  show_row_names = F,
+  
+  # column (sample) parameters
+  
+  cluster_column_slices = T,
   cluster_columns = T,
   show_column_dend = T,
   show_column_names = F,
@@ -180,11 +223,8 @@ heatmap <- Heatmap(
 
 # Export heatmap
 
-
-# Export heatmap of raw data
-
 png(
-  file = "./output/plots_heatmap/normalised_expression_genes_sample_ID.png",
+  file = "./output/plots_heatmap/normalised_expression_genes_clustered.png",
   width = 12,
   height = 12,
   units = "in",
@@ -192,7 +232,7 @@ png(
 )
 
 export_heatmap <- plot(
-  heatmap
+  heatmap_clustered
 )
 
 dev.off()
