58c93ea248a2ba10777ffd5b7480a1f78d12139b
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-08-28 10:27:05 +1000

    Change pathway for plot

--- scripts/7_post_camera_perform.R
+++ scripts/7_post_camera_perform.R
@@ -13,18 +13,18 @@ saveRDS(enrichments_camera, file.path('output',
                                       'data_enrichment',
                                       'camera',
                                       "camera_all.RDS"))
-
-# Barcode plots of selected pathways
-
-barcodeplot(topTable(fit_contrasts,
-                     coef = 15,
-                     number = Inf,
-                     sort.by = "none")$t,
-            index = msigdb_h$HALLMARK_TNFA_SIGNALING_VIA_NFKB)
-
-glimmaVolcano(x = fit_contrasts[msigdb_reactome$REACTOME_OLFACTORY_SIGNALING_PATHWAY,],
-              counts = quant_DGE_voom[msigdb_reactome$REACTOME_OLFACTORY_SIGNALING_PATHWAY,]$E,
-              groups = quant_DGE_voom[msigdb_reactome$REACTOME_OLFACTORY_SIGNALING_PATHWAY,]$targets$condition_ID,
-              xlab = "logFC",
-              transform.counts = "none",
-              coef = 1)
+# 
+# # Barcode plots of selected pathways
+# 
+# barcodeplot(topTable(fit_contrasts,
+#                      coef = 15,
+#                      number = Inf,
+#                      sort.by = "none")$t,
+#             index = msigdb_h$HALLMARK_TNFA_SIGNALING_VIA_NFKB)
+# 
+# glimmaVolcano(x = fit_contrasts[msigdb_reactome$REACTOME_OLFACTORY_SIGNALING_PATHWAY,],
+#               counts = quant_DGE_voom[msigdb_reactome$REACTOME_OLFACTORY_SIGNALING_PATHWAY,]$E,
+#               groups = quant_DGE_voom[msigdb_reactome$REACTOME_OLFACTORY_SIGNALING_PATHWAY,]$targets$condition_ID,
+#               xlab = "logFC",
+#               transform.counts = "none",
+#               coef = 1)
