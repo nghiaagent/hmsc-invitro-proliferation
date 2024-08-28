f195a384f9befa4e0db91013e5447812468e85d1
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-08-28 10:39:26 +1000

    Add list of files for DESeq2

--- scripts/9_pipeline_DGE.R
+++ scripts/9_pipeline_DGE.R
@@ -1,10 +1,19 @@
 # Run scripts of limma::voom pipeline
 
-list_files <- c(
+list_files_limma <- c(
   "0_meta_env_prep.R",
   "1_ensembl_voom_pre_quant_import.R",
   "1_ensembl_voom_pre_quality_control_filter.R",
   "2_ensembl_voom_dge.R"
 )
 
-walk(list_files, \ (x) source(here::here("scripts", x)))
+
+list_files_deseq2 <- c(
+  "0_meta_env_prep.R",
+  "1_ensembl_deseq2_pre_quant_import.R",
+  "2_ensembl_deseq2_dge.R"
+)
+
+walk(list_files_limma, \ (x) source(here::here("scripts", x)))
+
+walk(list_files_deseq2, \ (x) source(here::here("scripts", x)))
