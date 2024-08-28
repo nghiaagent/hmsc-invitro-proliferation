a7ef6c44584751037106cdba670b6d13793b7d57
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-08-28 10:39:13 +1000

    Use loops instead of repeating code to source files

--- scripts/9_pipeline_WGCNA.R
+++ scripts/9_pipeline_WGCNA.R
@@ -1,29 +1,14 @@
-# Load packages, perform DGE
+# Run scripts of WGCNA pipeline
 
-source(file.path(".",
-                 "scripts",
-                 "0_meta_env_prep.R"))
+list_files <- c(
+  "0_meta_env_prep.R",
+  "1_ensembl_voom_pre_quant_import.R",
+  "1_ensembl_voom_pre_quality_control_filter.R",
+  "2_ensembl_voom_dge.R",
+  "5_post_WGCNA_1a_load_and_QC_allsamples.R",
+  "5_post_WGCNA_2a_construct_net_allsamples.R",
+  "5_post_WGCNA_1b_load_and_QC_D3_UT_only.R",
+  "5_post_WGCNA_2b_construct_net_D3_UT_only.R"
+)
 
-source(file.path(".",
-                 "scripts",
-                 "2_dge.R"))
-
-# Perform WGCNA with all samples
-
-source(file.path(".",
-                 "scripts",
-                 "5_post_WGCNA_1a_load_and_QC_allsamples.R"))
-
-source(file.path(".",
-                 "scripts",
-                 "5_post_WGCNA_2a_construct_net_allsamples.R"))
-
-# Perform WGCNA on D3 UT samples only
-
-source(file.path(".",
-                 "scripts",
-                 "5_post_WGCNA_1b_load_and_QC_D3_UT_only.R"))
-
-source(file.path(".",
-                 "scripts",
-                 "5_post_WGCNA_2b_construct_net_D3_UT_only.R"))
+walk(list_files, \ (x) source(here::here("scripts", x)))
