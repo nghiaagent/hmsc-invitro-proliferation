e39fa2340de038e87ba36ba609143796e2c576d4
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-08-30 13:47:24 +1000

    Simplify code, change method for defining contrasts

--- scripts/2_ensembl_splitpopulation_deseq2_dge.R
+++ scripts/2_ensembl_splitpopulation_deseq2_dge.R
@@ -1,42 +1,55 @@
 ### Don't source this file by itself; call in from another file after running env_prep.R
 
-# Load dataset, split into 2 populations, subset to D3 run DESeq2 analysis
+# Load dataset, split into 2 populations, remove P13 D5 data to run DESeq2 analysis
 # Remember that experimental design is already embedded in the dataset and model.
-
-quant_big <- readRDS("./output/data_expression/pre_DGE/quant_cDNA_deseq.RDS")
-
-quant_cell_line <- c("hMSC-20176", "hMSC-21558") %>%
-  set_names() %>%
+quant_cell_line <- list(
+  "hMSC-20176" = "hMSC-20176",
+  "hMSC-21558" = "hMSC-21558"
+) %>%
+  
   # Split dataset based on cell population
-  map(.f = \(x) quant_big[, quant_big$cell_line == x & quant_big$timepoint_ID != "P13D5"]) %>%
-  # Refactor batch and cell line factors, drop levels not present in the population
-  map(.f = \(x) {
-    colData(x)$cell_line %<>% droplevels()
-    colData(x)$Passage %<>% droplevels()
-    colData(x)$Day %<>% droplevels()
-    colData(x)$Treatment %<>% droplevels()
-    colData(x)$run_date %<>% droplevels()
-    colData(x)$ID %<>% droplevels()
-    colData(x)$timepoint_ID %<>% droplevels()
-    colData(x)$condition_ID %<>% droplevels()
-    return(x)
-  }) %>%
-  # Update design
+  map(.x = .,
+      .f = \(x) {
+    readRDS("./output/data_expression/pre_DGE/quant_cDNA_deseq.RDS") %>%
+      .[, .$cell_line == x & .$timepoint_ID != "P13D5"]
+    }) %>%
+  
+  # Refactor batch and cell line factors, drop levels not present in the population. Update design
   map(.f = \(x) {
     design(x) <- ~ condition_ID + run_date
+    colData(x) %<>% droplevels()
+    return(x)
+  }) %>%
+  
+  # Update design
+  map(.f = \(x) {
     return(x)
     }) %>%
-  # Run DESeq2
+  
+  # Run DESeq
   map(.f = \(x) DESeq(x))
 
-res <- results(
-  quant_cell_line[[1]],
-  contrast = c("condition_ID", "P7D3Untreated", "P5D3Untreated"),
-  alpha = 0.05,
-  filterFun = ihw
-) %>%
-  as.data.frame() %>%
-  filter(padj < 0.05)
+results_20176 <- map(
+  list_contrasts_deseq2[c(1,3,5,13,14,15)],
+  \ (x) results(
+    quant_cell_line[["hMSC-20176"]],
+    contrast = x,
+    filterFun = ihw,
+    alpha = 0.05
+  ),
+  .progress = TRUE
+)
+
+results_21558 <- map(
+  list_contrasts_deseq2[c(1,3,5,13,14,15)],
+  \ (x) results(
+    quant_cell_line[["hMSC-21558"]],
+    contrast = x,
+    filterFun = ihw,
+    alpha = 0.05
+  ),
+  .progress = TRUE
+)
 
 # Save data
 
