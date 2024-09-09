7b3f3ac697763c4b98b289b5966866bc1b6a5065
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-09-09 13:51:20 +1000

    feat(data-processing): Implement batch correction and run DESeq2
    This commit implements batch correction using sva::ComBat_seq and runs DESeq2 on the
    batch-corrected gene expression data. The primary changes are:
    
    - Perform batch correction on the gene expression data, preserving the condition and
      cell line covariates.
    - Run DESeq2 on the batch-corrected data to obtain differential expression results.
    - Export the rlog-transformed data and the DESeq2 results for further downstream
      analysis.

--- scripts/2_ensembl_deseq2_dge.R
+++ scripts/2_ensembl_deseq2_dge.R
@@ -5,14 +5,7 @@
 
 quant_deseq2 <- readRDS("output/data_expression/pre_DGE/quant_cDNA_deseq.RDS")
 
-# Try batch correction
-
-## Ignore batch correction
-
-quant_deseq2_ignorebatch <- quant_deseq2
-
-design(quant_deseq2_ignorebatch) <- ~ condition_ID + run_date
-
+# Perform batch correction
 ## Batch correction + factor
 
 quant_deseq2_batchcor <- quant_deseq2
@@ -23,94 +16,50 @@ counts(quant_deseq2_batchcor) <- quant_deseq2_batchcor %$%
   sva::ComBat_seq(
     counts(.),
     batch = colData(.)$run_date
-    ## Uncomment the below to preserve covariates in batch correction.
-    ## Currently this produces NAs due to unbalanced design.
     ,
     covar_mod = model.matrix( ~ condition_ID + cell_line, data = colData(.))
   ) %>%
   `storage.mode<-`(., "integer")
 
-## Batch correction + no factor
+# Run DESeq2
 
-quant_deseq2_batchcor_nofactor <- quant_deseq2
-
-design(quant_deseq2_batchcor_nofactor) <- ~ condition_ID + cell_line
-
-counts(quant_deseq2_batchcor_nofactor) <- quant_deseq2_batchcor_nofactor %$%
-  sva::ComBat_seq(
-    counts(.),
-    batch = colData(.)$run_date
-    ## Uncomment the below to preserve covariates in batch correction.
-    ## Currently this produces NAs due to unbalanced design.
-    ,
-    covar_mod = model.matrix( ~ condition_ID + cell_line, data = colData(.))
-  ) %>%
-  `storage.mode<-`(., "integer")
-
-## Batch correction + no covariate
-
-quant_deseq2_batchcor_nocovar <- quant_deseq2
-
-design(quant_deseq2_batchcor_nocovar) <- ~ condition_ID + cell_line + run_date
-
-counts(quant_deseq2_batchcor_nocovar) <- quant_deseq2_batchcor_nocovar %$%
-  sva::ComBat_seq(
-    counts(.),
-    batch = colData(.)$run_date
-    ## Uncomment the below to preserve covariates in batch correction.
-    ## Currently this produces NAs due to unbalanced design.
-    # ,
-    # covar_mod = model.matrix( ~ condition_ID + cell_line, data = colData(.))
-  ) %>%
-  `storage.mode<-`(., "integer")
-
-# Run DESeq2 on datasets
-
-quant_deseq2 %<>% DESeq()
 quant_deseq2_batchcor %<>% DESeq()
-quant_deseq2_batchcor_nocovar %<>% DESeq()
-quant_deseq2_batchcor_nofactor %<>% DESeq()
-quant_deseq2_ignorebatch %<>% DESeq()
 
 # Obtain results
 
-## Compare between methods
-
 results <- map(
-  list(quant_deseq2,
-       quant_deseq2_batchcor,
-       quant_deseq2_batchcor_nocovar,
-       quant_deseq2_batchcor_nofactor,
-       quant_deseq2_ignorebatch
-),
+  list_contrasts_deseq2,
   \ (x) results(
-    x,
-    contrast = list_contrasts_deseq2[[15]],
+    quant_deseq2_batchcor,
+    contrast = x,
     filterFun = ihw,
     alpha = 0.05
   ),
   .progress = TRUE
 )
 
-# 
-# results <- map(
-#   list_contrasts_deseq2[c(13,14,15)],
-#   \ (x) results(
-#     quant_deseq2,
-#     contrast = x,
-#     filterFun = ihw,
-#     alpha = 0.05
-#   ),
-#   .progress = TRUE
-# )
-# 
-# results_batchcor <- map(
-#   list_contrasts_deseq2[c(13,14,15)],
-#   \ (x) results(
-#     quant_deseq2_batchcor,
-#     contrast = x,
-#     filterFun = ihw,
-#     alpha = 0.05
-#   ),
-#   .progress = TRUE
-# )
+# Export rlog for camera; WGCNA, etc.
+
+rlog_deseq2_batchcor <- rlog(quant_deseq2_batchcor)
+
+# Save data
+
+saveRDS(
+  quant_deseq2_batchcor,
+  file = here::here(
+    "output",
+    "data_expression",
+    "post_DGE",
+    "quant_deseq2_batchcor.RDS"
+  )
+)
+
+saveRDS(
+  rlog_deseq2_batchcor,
+  file = here::here("output", "data_expression", "post_DGE", "rlog_deseq2.RDS")
+)
+
+saveRDS(
+  results,
+  file = here::here("output", "data_expression", "post_DGE", "results_deseq2.RDS")
+)
