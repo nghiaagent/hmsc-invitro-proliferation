e40a267b8a349a7f69d9f1f3bf883a1ded8846f7
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-09-20 14:20:43 +1000

    feat: Add LRT to DESeq2 analysis and save results
    The changes in this commit include:
    
    1. Adds a second run of DESeq2 with a reduced model using the Likelihood Ratio Test
       (LRT) to get an ANOVA-like statistic for the 3D volcano plot.
    2. Saves the LRT results to an RDS file.
    3. Updates the file paths for saving the rlog-transformed counts and DESeq2
       results.
    
    These changes were made to improve the downstream analysis by providing
    additional statistical tests and organizing the outputs.

--- scripts/2_dge.R
+++ scripts/2_dge.R
@@ -14,26 +14,37 @@ design(quant_deseq2_batchcor) <- ~ condition_ID + cell_line + run_date
 counts(quant_deseq2_batchcor) <- quant_deseq2_batchcor %$%
   sva::ComBat_seq(
     counts(.),
-    batch = colData(.)$run_date
-    ,
-    covar_mod = model.matrix( ~ condition_ID + cell_line, data = colData(.))
+    batch = colData(.)$run_date,
+    covar_mod = model.matrix(~ condition_ID + cell_line, data = colData(.))
   ) %>%
   `storage.mode<-`(., "integer")
 
 # Run DESeq2
+## Run once with full model
+## Run again with reduced model and LRT
+## to get ANOVA-like statistic for 3D volcano plot
+## (i.e. chance of gene differentially expressed at all)
 
-quant_deseq2_batchcor %<>% DESeq()
+quant_deseq2_batchcor %<>% DESeq() # nolint: assignment_linter.
+
+quant_deseq2_lrt <- quant_deseq2_batchcor %>%
+  DESeq(
+    test = "LRT",
+    reduced = ~ cell_line + run_date
+  )
 
 # Obtain results
 
 results <- map(
   list_contrasts_deseq2,
-  \ (x) results(
-    quant_deseq2_batchcor,
-    contrast = x,
-    filterFun = ihw,
-    alpha = 0.05
-  ),
+  \(x) {
+    results(
+      quant_deseq2_batchcor,
+      contrast = x,
+      filterFun = ihw,
+      alpha = 0.05
+    )
+  },
   .progress = TRUE
 )
 
@@ -54,11 +65,31 @@ saveRDS(
 )
 
 saveRDS(
+  quant_deseq2_lrt,
+  file = here::here(
+    "output",
+    "data_expression",
+    "post_DGE",
+    "quant_deseq2_LRT.RDS"
+  )
+)
+
+saveRDS(
   rlog_deseq2_batchcor,
-  file = here::here("output", "data_expression", "post_DGE", "rlog_deseq2.RDS")
+  file = here::here(
+    "output",
+    "data_expression",
+    "post_DGE",
+    "rlog_deseq2.RDS"
+  )
 )
 
 saveRDS(
   results,
-  file = here::here("output", "data_expression", "post_DGE", "results_deseq2.RDS")
+  file = here::here(
+    "output",
+    "data_expression",
+    "post_DGE",
+    "results_deseq2.RDS"
+  )
 )
