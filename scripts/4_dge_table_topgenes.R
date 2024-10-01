f69ae60bdc2f15e65ee28c0e08ef6be812784717
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-10-01 15:45:32 +1000

    Rewrite code to make use of new results table

--- scripts/4_dge_table_topgenes.R
+++ scripts/4_dge_table_topgenes.R
@@ -1,68 +1,56 @@
-# Extract logFC and p-vals for comparisons between passages at D3 and D5
+# Load data
 
-top_P7vsP5_D3 <- topTable(fit_contrasts, coef = 4,
-                          number = Inf,
-                          sort.by = "p")
-
-top_P7vsP5_D5 <- topTable(fit_contrasts, coef = 13,
-                          number = Inf,
-                          sort.by = "p")
-
-top_P13vsP5_D3 <- topTable(fit_contrasts, coef = 5,
-                           number = Inf,
-                           sort.by = "p")
-
-top_P13vsP5_D5 <- topTable(fit_contrasts, coef = 14,
-                           number = Inf,
-                           sort.by = "p")
-
-top_P13vsP7_D3 <- topTable(fit_contrasts, coef = 6,
-                           number = Inf,
-                           sort.by = "p")
-
-top_P13vsP7_D5 <- topTable(fit_contrasts, coef = 15,
-                           number = Inf,
-                           sort.by = "p")
-
-# Extract logFC and p-vals for Trt vs Ctrl
-
-top_treat_P5_D3 <- topTable(fit_contrasts, coef = 1,
-                          number = Inf,
-                          sort.by = "p")
-
-top_treat_P5_D5 <- topTable(fit_contrasts, coef = 10,
-                          number = Inf,
-                          sort.by = "p")
-
-top_treat_P7_D3 <- topTable(fit_contrasts, coef = 2,
-                           number = Inf,
-                           sort.by = "p")
-
-top_treat_P7_D5 <- topTable(fit_contrasts, coef = 11,
-                           number = Inf,
-                           sort.by = "p")
-
-top_treat_P13_D3 <- topTable(fit_contrasts, coef = 3,
-                           number = Inf,
-                           sort.by = "p")
-
-top_treat_P13_D5 <- topTable(fit_contrasts, coef = 12,
-                           number = Inf,
-                           sort.by = "p")
-
-list_sheets <- list(
-  "Phase B - A, D3" = top_P7vsP5_D3,
-  "Phase C - A, D3" = top_P13vsP5_D3,
-  "Phase C - B, D3" = top_P13vsP7_D3,
-  "Phase B - A, D5" = top_P7vsP5_D5,
-  "Phase C - A, D5" = top_P13vsP5_D5,
-  "Phase C - B, D5" = top_P13vsP7_D5,
-  "Treatment @ Phase A, D3" = top_treat_P5_D3,
-  "Treatment @ Phase B, D3" = top_treat_P7_D3,
-  "Treatment @ Phase C, D3" = top_treat_P13_D3,
-  "Treatment @ Phase A, D5" = top_treat_P5_D5,
-  "Treatment @ Phase B, D5" = top_treat_P7_D5,
-  "Treatment @ Phase C, D5" = top_treat_P13_D5
+results <- readRDS(
+  file = here::here(
+    "output",
+    "data_expression",
+    "post_DGE",
+    "results_deseq2.RDS"
+  )
 )
 
-write_xlsx(list_sheets, "./output/top_DEGs.xlsx")
+quant_deseq2 <- readRDS(
+  file = here::here(
+    "output",
+    "data_expression",
+    "post_DGE",
+    "quant_deseq2_batchcor.RDS"
+  )
+)
+
+# Extract logFC and p-vals for comparisons between passages at D3 and D5
+# Review env prep file for info of which coefficient represent which contrasts
+# Compile to list, coerce to data.frame, export to excel
+
+gene_info <- rowRanges(quant_deseq2) %>%
+  as.data.frame() %>%
+  .[, c(6, 7, 8, 10)]
+
+list_sheets <- results %$%
+  list(
+    "P+7 - P+5, D3" = .[[13]],
+    "P+13 - P+5, D3" = .[[15]],
+    "P+13 - P+7, D3" = .[[14]],
+    "P+7 - P+5, D5" = .[[19]],
+    "P+13 - P+5, D5" = .[[21]],
+    "P+13 - P+7, D5" = .[[20]],
+    "Treatment @ P+5, D3" = .[[1]],
+    "Treatment @ P+7, D3" = .[[3]],
+    "Treatment @ P+13, D3" = .[[5]],
+    "Treatment @ P+5, D5" = .[[2]],
+    "Treatment @ P+7, D5" = .[[4]],
+    "Treatment @ P+13, D5" = .[[6]]
+  ) %>%
+  map(.f = \(x) {
+    as.data.frame(x) %>%
+      cbind(gene_info, .)
+  })
+
+write.xlsx(
+  x = list_sheets,
+  file = here::here(
+    "output",
+    "top_DEGs.xlsx"
+  ),
+  asTable = TRUE
+)
