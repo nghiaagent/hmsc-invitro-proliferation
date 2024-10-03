29a265148f7b13a3af76cb249b5b0001487020b5
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-10-03 16:39:21 +1000

    Add more comparisons to be extracted, export all data, rename files to be more relevant

--- scripts/4_dge_table_topgenes.R
+++ scripts/4_dge_table_topgenes.R
@@ -87,18 +87,83 @@ list_sheets <- results_lfcshrink %$%
     "Treatment @ P+13, D3" = .[[5]],
     "Treatment @ P+5, D5" = .[[2]],
     "Treatment @ P+7, D5" = .[[4]],
-    "Treatment @ P+13, D5" = .[[6]]
-  ) %>%
-  map(.f = \(x) {
-    as.data.frame(x) %>%
-      cbind(gene_info, .)
-  })
+    "Treatment @ P+13, D5" = .[[6]],
+    "D5 - D3, P+5 Ctl" = .[[7]],
+    "D5 - D3, P+5 Hep" = .[[10]],
+    "D5 - D3, P+7 Ctl" = .[[8]],
+    "D5 - D3, P+7 Hep" = .[[11]],
+    "D5 - D3, P+13 Ctl" = .[[9]],
+    "D5 - D3, P+13 Hep" = .[[12]]
+  )
+
+# Extract logFC and p-vals for comparisons between passages at D3 and D5
+# Compile to list, coerce to data.frame, export to excel
+# 2 lists: Contrasts and LRT
+
+## All contrasts (Wald test)
 
 write.xlsx(
-  x = list_sheets,
+  x = map(
+    list_sheets,
+    \(x) {
+      extract_topgenes(
+        results = x,
+        dds = quant_deseq2,
+        ntop = Inf,
+        signif_only = FALSE
+      )
+    },
+    .progress = TRUE
+  ),
   file = here::here(
     "output",
-    "top_DEGs.xlsx"
+    "genes_all_contrasts.xlsx"
   ),
   asTable = TRUE
+)
+
+## Omnibus (LRT)
+
+write.xlsx(
+  x = results(quant_deseq2_lrt) %>%
+    extract_topgenes(
+      results = .,
+      dds = quant_deseq2,
+      ntop = Inf,
+      signif_only = FALSE
+    ) %>%
+    dplyr::arrange(`adj. P-val`) %>%
+    dplyr::select(!LogFC),
+  file = here::here(
+    "output",
+    "genes_all_LRT.xlsx"
+  ),
+  asTable = TRUE
+)
+
+# Extract top and bottom 20 genes for desired comparisons
+# Compile to list, export to excel
+# 2 lists: all genes and DEGs only
+
+map2(
+  c(TRUE, FALSE),
+  c("genes_top_DE.xlsx", "genes_top_LFC.xlsx"),
+  \(signif_only, filename) {
+    write.xlsx(
+      x = map(
+        list_sheets,
+        \(x) {
+          extract_topgenes(
+            x,
+            quant_deseq2,
+            ntop = 20,
+            signif_only = signif_only
+          )
+        },
+        .progress = TRUE
+      ),
+      file = here::here("output", filename),
+      asTable = TRUE
+    )
+  }
 )
