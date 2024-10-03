4637786b4aa2d144dc146a2a0e690c500e8b497f
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-10-03 16:36:23 +1000

    Adjust indentation

--- scripts/1_pre_expressionheatmap.R
+++ scripts/1_pre_expressionheatmap.R
@@ -1,5 +1,5 @@
 # Call DGE scripts first before calling this script
-# Plots normalised log2CPM (quant_DGE_voom) as a heatmap. 
+# Plots normalised log2CPM (quant_DGE_voom) as a heatmap.
 # Use ?voom to check how it's made.
 # limma authors recommend using logCPM from cpm() or voom()
 # https://www.biostars.org/p/9511346/
@@ -45,53 +45,53 @@ anno <- tibble(
 
 anno_cols <- list(
   `Cell population` = c(
-    'hMSC-20176' = col_cell_line[1],
-    'hMSC-21558' = col_cell_line[2]
+    "hMSC-20176" = col_cell_line[1],
+    "hMSC-21558" = col_cell_line[2]
   ),
-  `Passage` = c('P5' = col_passage[1], 'P7' = col_passage[2], 'P13' = col_passage[3]),
-  `Day` = c('D3' = col_day[1], 'D5' = col_day[2]),
-  `Treatment` = c('Untreated' = col_treatment[1], 'Treated' = col_treatment[2])
+  `Passage` = c("P5" = col_passage[1], "P7" = col_passage[2], "P13" = col_passage[3]),
+  `Day` = c("D3" = col_day[1], "D5" = col_day[2]),
+  `Treatment` = c("Untreated" = col_treatment[1], "Treated" = col_treatment[2])
 )
 
 ## ComplexHeatmap metadata annotation object
 
 anno_object <- HeatmapAnnotation(
   df = anno,
-  which = 'col',
+  which = "col",
   col = anno_cols,
   annotation_height = 0.6,
   annotation_legend_param = list(
     `Cell population` = list(
       nrow = 2,
-      title = 'Cell population',
-      title_position = 'topleft',
-      legend_direction = 'vertical',
-      title_gp = gpar(fontsize = 12, fontface = 'bold'),
-      labels_gp = gpar(fontsize = 12, fontface = 'bold')
+      title = "Cell population",
+      title_position = "topleft",
+      legend_direction = "vertical",
+      title_gp = gpar(fontsize = 12, fontface = "bold"),
+      labels_gp = gpar(fontsize = 12, fontface = "bold")
     ),
     `Passage` = list(
       nrow = 3,
-      title = 'Passage',
-      title_position = 'topleft',
-      legend_direction = 'vertical',
-      title_gp = gpar(fontsize = 12, fontface = 'bold'),
-      labels_gp = gpar(fontsize = 12, fontface = 'bold')
+      title = "Passage",
+      title_position = "topleft",
+      legend_direction = "vertical",
+      title_gp = gpar(fontsize = 12, fontface = "bold"),
+      labels_gp = gpar(fontsize = 12, fontface = "bold")
     ),
     `Day` = list(
       nrow = 2,
-      title = 'Day',
-      title_position = 'topleft',
-      legend_direction = 'vertical',
-      title_gp = gpar(fontsize = 12, fontface = 'bold'),
-      labels_gp = gpar(fontsize = 12, fontface = 'bold')
+      title = "Day",
+      title_position = "topleft",
+      legend_direction = "vertical",
+      title_gp = gpar(fontsize = 12, fontface = "bold"),
+      labels_gp = gpar(fontsize = 12, fontface = "bold")
     ),
     `Treatment group` = list(
       nrow = 2,
-      title = 'Treatment',
-      title_position = 'topleft',
-      legend_direction = 'vertical',
-      title_gp = gpar(fontsize = 12, fontface = 'bold'),
-      labels_gp = gpar(fontsize = 12, fontface = 'bold')
+      title = "Treatment",
+      title_position = "topleft",
+      legend_direction = "vertical",
+      title_gp = gpar(fontsize = 12, fontface = "bold"),
+      labels_gp = gpar(fontsize = 12, fontface = "bold")
     )
   )
 )
@@ -102,69 +102,74 @@ anno_genelabels <- rowAnnotation(
   Genes = anno_mark(
     at = seq(1, nrow(E_heatmap), 60),
     labels = rownames(E_heatmap)[seq(1, nrow(E_heatmap), 60)],
-    labels_gp = gpar(fontsize = 8, fontface = 'bold'),
+    labels_gp = gpar(fontsize = 8, fontface = "bold"),
     padding = 0.75
   ),
-  width = unit(2.0, 'cm') +
-    
+  width = unit(2.0, "cm") +
+
     max_text_width(rownames(E_heatmap)[seq(1, nrow(E_heatmap), 60)],
-                   gp = gpar(
-                     fontsize = 8,  fontface = 'bold'
-                   ))
+      gp = gpar(
+        fontsize = 8, fontface = "bold"
+      )
+    )
 )
 
-# Apply row clustering 
+# Apply row clustering
 
 clusters <- pam(E_heatmap,
-                k = 5)
+  k = 5
+)
 
-clusters$clustering <- paste0('Cluster ', 
-                              clusters$clustering)
+clusters$clustering <- paste0(
+  "Cluster ",
+  clusters$clustering
+)
 
 clusters$clustering <- factor(clusters$clustering,
-                              levels = c(
-                                'Cluster 1',
-                                'Cluster 2',
-                                'Cluster 3',
-                                'Cluster 4',
-                                'Cluster 5'
-                              ))
+  levels = c(
+    "Cluster 1",
+    "Cluster 2",
+    "Cluster 3",
+    "Cluster 4",
+    "Cluster 5"
+  )
+)
 
 # Create heatmap object
 
 heatmap <- Heatmap(
   E_heatmap,
-  name = 'Gene\nZ-\nscore',
+  name = "Gene\nZ-\nscore",
   col = colorRamp2(breaks, col),
   border = F,
-  
+
   # Group rows based on clusters
-  
+
   row_split = clusters$clustering,
   cluster_row_slices = FALSE,
-  
+
   # parameters for the colour-bar that represents gradient of expression
-  
+
   heatmap_legend_param = list(
-    color_bar = 'continuous',
-    legend_direction = 'vertical',
-    legend_width = unit(8, 'cm'),
-    legend_height = unit(5.0, 'cm'),
-    title_position = 'topcenter',
-    title_gp = gpar(fontsize = 12, fontface = 'bold'),
-    labels_gp = gpar(fontsize = 12, fontface = 'bold')
+    color_bar = "continuous",
+    legend_direction = "vertical",
+    legend_width = unit(8, "cm"),
+    legend_height = unit(5.0, "cm"),
+    title_position = "topcenter",
+    title_gp = gpar(fontsize = 12, fontface = "bold"),
+    labels_gp = gpar(fontsize = 12, fontface = "bold")
   ),
-  
+
   # row (gene) parameters
-  
+
   cluster_rows = T,
-  show_row_dend = T,      
+  show_row_dend = T,
   row_title = levels(clusters$clustering),
-  row_title_side = 'left',
-  row_title_gp = gpar(fontsize = 10,  fontface = 'bold'),
+  row_title_side = "left",
+  row_title_gp = gpar(fontsize = 10, fontface = "bold"),
   row_title_rot = 90,
   show_row_names = F,
-  
+
   # column (sample) parameters
 
   cluster_column_slices = F,
@@ -172,9 +177,9 @@ heatmap <- Heatmap(
   cluster_columns = T,
   show_column_dend = T,
   show_column_names = F,
-  
+
   # specify top and bottom annotations
-  
+
   top_annotation = anno_object
 )
 
@@ -196,4 +201,3 @@ export_heatmap <- plot(
 )
 
 dev.off()
-
