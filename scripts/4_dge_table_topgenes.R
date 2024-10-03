0abeb53e50e893b1a5716d0e9ca39096d84d7051
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-10-03 16:38:31 +1000

    Define function to extract top genes from DESeqResults, switch to using shrunken LFC in line with volcano2d plots

--- scripts/4_dge_table_topgenes.R
+++ scripts/4_dge_table_topgenes.R
@@ -1,32 +1,80 @@
-# Load data
+# Define function to return top and bottom genes of a DESeqResults object by LFC
+# Or return all genes if asked.
 
-results <- readRDS(
-  file = here::here(
-    "output",
-    "data_expression",
-    "post_DGE",
-    "results_deseq2.RDS"
+extract_topgenes <- function(results, dds, ntop = Inf, signif_only = TRUE) {
+  # Format results and gene metadata
+  results_format <- results %>%
+    as_tibble(rownames = "gene_id") %>%
+    dplyr::select(c(gene_id, log2FoldChange, padj))
+
+  row_ranges_format <- rowRanges(dds) %>%
+    as_tibble() %>%
+    dplyr::select(c(gene_id, gene_name, description))
+
+  # Merge into one table, sort by decreasing fold change
+  results_merge <- right_join(
+    row_ranges_format, results_format,
+    by = join_by(gene_id == gene_id)
+  ) %>%
+    drop_na() %>%
+    arrange(desc(log2FoldChange))
+
+  # Remove non-significant genes
+  if (signif_only == TRUE) {
+    results_merge %<>% filter(padj < 0.05)
+  }
+
+  results_merge %<>% dplyr::rename(
+    "ENSEMBL ID" = gene_id,
+    "Symbol" = gene_name,
+    "Gene name" = description,
+    "LogFC" = log2FoldChange,
+    "adj. P-val" = padj
   )
-)
+
+  # Return split table (or not) depending on number of top genes provided
+  # If not provided: Return entire table
+  # If smaller number of top genes, return table containing top and bottom genes
+
+  if (ntop == Inf) {
+    results_merge %>%
+      return()
+  } else {
+    results_merge %$%
+      rbind(
+        filter(., `LogFC` > 0) %>% slice_head(n = ntop),
+        filter(., `LogFC` < 0) %>% slice_tail(n = ntop)
+      ) %>%
+      return()
+  }
+}
+
+# Load data
 
 quant_deseq2 <- readRDS(
   file = here::here(
-    "output",
-    "data_expression",
-    "post_DGE",
+    "output", "data_expression", "post_DGE",
     "quant_deseq2_batchcor.RDS"
   )
 )
 
-# Extract logFC and p-vals for comparisons between passages at D3 and D5
-# Review env prep file for info of which coefficient represent which contrasts
-# Compile to list, coerce to data.frame, export to excel
+quant_deseq2_lrt <- readRDS(
+  file = here::here(
+    "output", "data_expression", "post_DGE",
+    "quant_deseq2_LRT.RDS"
+  )
+)
 
-gene_info <- rowRanges(quant_deseq2) %>%
-  as.data.frame() %>%
-  .[, c(6, 7, 8, 10)]
+results_lfcshrink <- readRDS(
+  file = here::here(
+    "output", "data_expression", "post_DGE",
+    "results_deseq2_lfcshrink.RDS"
+  )
+)
 
-list_sheets <- results %$%
+# Define list of results to be exported
+
+list_sheets <- results_lfcshrink %$%
   list(
     "P+7 - P+5, D3" = .[[13]],
     "P+13 - P+5, D3" = .[[15]],
