88bc10a2de262c7b8a288a1d27972f5cc84ac185
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-10-10 14:37:39 +1000

    Move function, adjust output

--- scripts/4_dge_table_topgenes.R
+++ scripts/4_dge_table_topgenes.R
@@ -1,54 +1,3 @@
-# Define function to return top and bottom genes of a DESeqResults object by LFC
-# Or return all genes if asked.
-
-extract_topgenes <- function(results, dds, ntop = Inf, signif_only = TRUE) {
-  # Format results and gene metadata
-  results_format <- results %>%
-    as_tibble(rownames = "gene_id") %>%
-    dplyr::select(c(gene_id, log2FoldChange, padj))
-
-  row_ranges_format <- rowRanges(dds) %>%
-    as_tibble() %>%
-    dplyr::select(c(gene_id, gene_name, description))
-
-  # Merge into one table, sort by decreasing fold change
-  results_merge <- right_join(
-    row_ranges_format, results_format,
-    by = join_by(gene_id == gene_id)
-  ) %>%
-    drop_na() %>%
-    arrange(desc(log2FoldChange))
-
-  # Remove non-significant genes
-  if (signif_only == TRUE) {
-    results_merge %<>% filter(padj < 0.05)
-  }
-
-  results_merge %<>% dplyr::rename(
-    "ENSEMBL ID" = gene_id,
-    "Symbol" = gene_name,
-    "Gene name" = description,
-    "LogFC" = log2FoldChange,
-    "adj. P-val" = padj
-  )
-
-  # Return split table (or not) depending on number of top genes provided
-  # If not provided: Return entire table
-  # If smaller number of top genes, return table containing top and bottom genes
-
-  if (ntop == Inf) {
-    results_merge %>%
-      return()
-  } else {
-    results_merge %$%
-      rbind(
-        filter(., `LogFC` > 0) %>% slice_head(n = ntop),
-        filter(., `LogFC` < 0) %>% slice_tail(n = ntop)
-      ) %>%
-      return()
-  }
-}
-
 # Load data
 
 quant_deseq2 <- readRDS(
@@ -117,6 +66,7 @@ write.xlsx(
   ),
   file = here::here(
     "output",
+    "data_expression",
     "genes_all_contrasts.xlsx"
   ),
   asTable = TRUE
@@ -136,6 +86,7 @@ write.xlsx(
     dplyr::select(!LogFC),
   file = here::here(
     "output",
+    "data_expression",
     "genes_all_LRT.xlsx"
   ),
   asTable = TRUE
@@ -162,7 +113,7 @@ map2(
         },
         .progress = TRUE
       ),
-      file = here::here("output", filename),
+      file = here::here("output", "data_expression", filename),
       asTable = TRUE
     )
   }
