f09447cacaaecdb32c5758ee7df3bf0ab8c01fc5
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2024-11-20 15:04:32 +1000

    Split code, create barcode plots

---
+++ scripts/6_post_camera_plot.R
@@ -0,0 +1,77 @@
+# Load data
+
+results <- readRDS(
+    file = here::here(
+        "output",
+        "data_expression",
+        "post_DGE",
+        "results_deseq2.RDS"
+    )
+)
+
+# Plot barcode plots for Hallmark gene sets
+
+## Plot multiple plots using a nested map
+## Inner map: Create barcodeplot of all gene sets on provided contrast
+## Outer map: Perform inner map on all contrasts
+
+list_barcodeplots <- imap(
+    results,
+    \(results, name_contrast) {
+        # Set and create output directory
+        path_output <- here::here(
+            "output",
+            "data_enrichment",
+            "camera",
+            name_contrast,
+            "barcodeplots"
+        )
+
+        if (!dir.exists(path_output)) {
+            dir.create(path_output, recursive = TRUE)
+        }
+
+        # Create and draw plot
+        imap(
+            list_gmt$h,
+            \(genesets, name_geneset) {
+                # Set up output device
+                png(
+                    filename = here::here(
+                        path_output,
+                        str_c(name_geneset, ".png", sep = "")
+                    ),
+                    res = 300,
+                    width = 7,
+                    height = 4,
+                    units = "in"
+                )
+
+                par(mar = c(4.5, 3, 1.5, 1) + 0.1)
+
+                # Draw plot
+                barcodeplot(
+                    results$log2FoldChange,
+                    index = genesets
+                )
+
+                # Export plot and object
+                x <- recordPlot()
+                dev.off()
+                return(x)
+            },
+            .progress = TRUE
+        )
+    },
+    .progress = TRUE
+)
+
+saveRDS(
+    list_barcodeplots,
+    file = here::here(
+        "output",
+        "data_enrichment",
+        "camera",
+        "camera_barcodeplots.RDS"
+    )
+)
