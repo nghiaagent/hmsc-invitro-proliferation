c9f44b445be8a35c125a0d91daaaec95fd1c482c
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2025-11-27 15:29:55 +1000

    feat(pipeline): Improve script sourcing indentation in DGE pipeline
    Adjust indentation for better readability in the script sourcing
    section of the DESeq2 pipeline workflow

--- scripts/9_pipeline_DGE.R
+++ scripts/9_pipeline_DGE.R
@@ -5,10 +5,10 @@ source("scripts/0_meta_env_prep.R", echo = TRUE, verbose = TRUE)
 # Run scripts of DESeq2 pipeline
 
 walk(
-  c(
-    "1_pre_quant_import.R",
-    "2_dge.R",
-    "4_dge_plot_volcano2D.R"
-  ),
-  \(x) source(here::here("scripts", x), echo = TRUE, verbose = TRUE)
+    c(
+        "1_pre_quant_import.R",
+        "2_dge.R",
+        "4_dge_plot_volcano2D.R"
+    ),
+    \(x) source(here::here("scripts", x), echo = TRUE, verbose = TRUE)
 )
