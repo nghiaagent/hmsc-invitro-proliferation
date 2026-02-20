ad843b76a066d71e5ee041a560439e0181581014
Nghia Nguyen (Mitchell) <nghia6a1@gmail.com>
2026-02-20 12:42:12 +1000

    Add heading here::i_am statement and package loading, reorganise code

--- R/1_pre_01_quant_import.R
+++ R/1_pre_01_quant_import.R
@@ -1,32 +1,32 @@
-### Don't source this file by itself;
-### call in from another file after running env_prep.R
-### Source
-### Import salmon transcript quantification results
-### to a single matrix, export that matrix.
+here::i_am("R/1_pre_01_quant_import.R")
+
+########################
+# This file imports the transcript quantification results from Salmon
+# Summarise to gene level
+# Filters out lowly abundant transcripts
+# and exports them to a single matrix.
+########################
+
+# Import packages
+library(DESeq2)
+library(here)
+library(tidyverse)
+library(tximeta)
 
 # Construct sample table
-table_samples <- here::here(
-  "input",
-  "annotation",
-  "Cell_sample_table.csv"
-) %>%
+table_samples <- here::here("input/annotation/cell_sample_table.csv") %>%
   read_csv() %>%
   mutate(
+    # Create ID column
     ID = name,
     .keep = "unused"
   ) %>%
   mutate(
+    # Format ID column to be consistent
     ID = ID %>%
-      str_replace(
-        "hMSC_",
-        "hMSC-"
-      ) %>%
-      str_replace(
-        "(?<=0)_(?=[:digit:])",
-        "-"
-      )
-  ) %>%
-  mutate(
+      str_replace("hMSC_", "hMSC-") %>%
+      str_replace("(?<=0)_(?=[:digit:])", "-"),
+    # Format factor columns in the correct levels
     cell_line = cell_line %>%
       factor(levels = c("hMSC-20176", "hMSC-21558")),
     Passage = Passage %>%
@@ -34,16 +34,13 @@ table_samples <- here::here(
     Day = Day %>%
       factor(levels = c("D3", "D5")),
     Treatment = Treatment %>%
-      factor(levels = c("Untreated", "Treated"))
-  ) %>%
-  mutate(
+      factor(levels = c("Untreated", "Treated")),
+    # Remove underscores from run_date
     run_date = run_date %>%
-      str_replace_all(
-        "_",
-        ""
-      )
+      str_replace_all("_", "")
   ) %>%
   mutate(
+    # Create timepoint_ID and condition_ID columns and set their order
     timepoint_ID = str_c(Passage, Day, sep = "") %>%
       factor(
         levels = c(
@@ -54,9 +51,7 @@ table_samples <- here::here(
           "P13D3",
           "P13D5"
         )
-      )
-  ) %>%
-  mutate(
+      ),
     condition_ID = str_c(Passage, Day, Treatment, sep = "") %>%
       factor(
         levels = c(
@@ -75,20 +70,22 @@ table_samples <- here::here(
         )
       )
   ) %>%
+  # Select samples that are selected to be
+  # included in the dataset (based on samplesheet)
   subset(included_in_dataset == TRUE) %>%
+  # Arrange samples in the correct order
   arrange(ID, cell_line, Passage, Day, Treatment) %>%
   mutate(
+    # Set ID and run_date as factors in the correct order
     ID = ID %>%
       factor() %>%
-      fct_inorder()
-  ) %>%
-  mutate(
+      fct_inorder(),
     run_date = run_date %>%
       factor()
   )
 
-# Construct transcriptome dataset for limma-voom
-## Construct this for cDNA data from ENSEMBL
+# Construct samplesheet that also includes file paths
+## Get list of salmon quantification files
 files_tx_quant <- table_samples$filename %>%
   str_c(
     "./input/cDNA/",
@@ -96,11 +93,13 @@ files_tx_quant <- table_samples$filename %>%
     "_quant/quant.sf"
   )
 
+## Get list of sample names from the files
 names_tx_quant <- files_tx_quant %>%
   str_replace(pattern = "IonXpress.*$", "") %>%
   str_replace(pattern = "./input/cDNA/", "") %>%
   str_replace(pattern = "_$", "")
 
+## Combine list of files and sample names as a tibble, add metadata
 list_files <- tibble(files = files_tx_quant, names = names_tx_quant) %>%
   cbind(dplyr::select(
     table_samples,
@@ -127,8 +126,7 @@ quant_deseq2 <- list_files %>%
   DESeqDataSet(design = ~ condition_ID + run_date + cell_line)
 
 # Filter lowly-expressed genes
-# Keep only genes with higher than 10 counts in at least 3 samples
-# Doesn't affect results; but speeds up computation
+# Keep only genes with higher than 40 counts in at least 3 samples
 keep <- quant_deseq2 %$%
   filterByExpr(
     y = counts(.),
